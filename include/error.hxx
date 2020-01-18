fb0736267acc186ddec4122167bb958647cb2548
amir5200fx <aasoleimani86@gmail.com>
2020-01-17 21:02:10 -0800

    compiling the geo lib

--- include/error.hxx
+++ include/error.hxx
@@ -239,17 +239,80 @@ namespace tnbLib
 	};
 
 
+	class ConvError
+		: public error
+	{
+
+		/*Private Data*/
+
+		label nbIterations_;
+		scalar theTolerance_;
+
+	public:
+
+		ConvError(const string& title)
+			: error(title)
+		{}
+
+		virtual ~ConvError() throw()
+		{}
+
+		label NbIterations() const
+		{
+			return nbIterations_;
+		}
+
+		scalar Tolerance() const
+		{
+			return theTolerance_;
+		}
+
+		void SetNbIterations(const label value)
+		{
+			nbIterations_ = value;
+		}
+
+		void SetTolerance(const scalar value)
+		{
+			theTolerance_ = value;
+		}
+
+		OSstream& operator()
+			(
+				const char* functionName,
+				const char* sourceFileName,
+				const int sourceFileLineNumber,
+				const label nbIterations,
+				const scalar tolerance
+				);
+
+		//- Exit : can be called for any error to exit program
+		void exit(const int errNo = 1);
+
+		//- Abort : used to stop code for fatal errors
+		void abort();
+
+
+		// Ostream operator
+
+		friend Ostream& operator<<(Ostream&, const ConvError&);
+	};
+
 	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 	// Global error declarations: defined in error.cxx
 
 	extern error FatalError;
 	extern IOerror FatalIOError;
+	extern ConvError FatalConvError;
 
 	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 	// Convenient macros to add the file name and line number to the function name
 
 #define FatalErrorIn(fn) FatalError(fn, __FILE__, __LINE__)
 #define FatalIOErrorIn(fn, ios) FatalIOError(fn, __FILE__, __LINE__, ios)
+#define FatalConvErrorIn(fn, nbIters, tol) FatalConvError(fn, __FILE__, __LINE__, nbIters, tol)
+
+#include <errorM.hxx>
 
 // Call for functions which are not currently implemented.
 // The functionName is printed and then abort is called.
