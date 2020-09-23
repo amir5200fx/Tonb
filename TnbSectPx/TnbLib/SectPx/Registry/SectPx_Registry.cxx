5e86db00ee0b69086d0a19eb95b62db0da4925cb
aasoleimani <aasoleimani86@gmail.com>
2020-09-23 11:34:14 -0700

    adding registry

---
+++ TnbSectPx/TnbLib/SectPx/Registry/SectPx_Registry.cxx
@@ -0,0 +1,201 @@
+#include <SectPx_Registry.hxx>
+
+#include <SectPx_RegObj.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+std::shared_ptr<tnbLib::SectPx_RegObj>
+tnbLib::SectPx_Registry::RemoveFromMap
+(
+	const Standard_Integer theIndex
+)
+{
+	auto iter = theObjects_.find(theIndex);
+	if (iter IS_EQUAL theObjects_.end())
+	{
+		FatalErrorIn
+		(
+			"std::shared_ptr<SectPx_RegObj> RemoveFromMap(const Standard_Integer theIndex)"
+		)
+			<< "the object is not in the tree!" << endl
+			<< " index = " << theIndex << endl
+			<< abort(FatalError);
+	}
+	auto item = std::move(iter->second);
+	theObjects_.erase(iter);
+	return std::move(item);
+}
+
+void tnbLib::SectPx_Registry::Add
+(
+	const Standard_Integer theIndex,
+	const std::shared_ptr<SectPx_RegObj>& theObj
+)
+{
+	auto paired = std::make_pair(theIndex, theObj);
+	auto insert = theObjects_.insert(std::move(paired));
+	if (NOT insert.second)
+	{
+		FatalErrorIn
+		(
+			"void Add(const Standard_Integer, const std::shared_ptr<SectPx_RegObj>&)"
+		)
+			<< "duplicate data!" << endl
+			<< " index = " << theIndex << endl
+			<< abort(FatalError);
+	}
+}
+
+void tnbLib::SectPx_Registry::Add
+(
+	const Standard_Integer theIndex,
+	std::shared_ptr<SectPx_RegObj>&& theObj
+)
+{
+	auto paired = std::make_pair(theIndex, std::move(theObj));
+	auto insert = theObjects_.insert(std::move(paired));
+	if (NOT insert.second)
+	{
+		FatalErrorIn
+		(
+			"void Add(const Standard_Integer, const std::shared_ptr<SectPx_RegObj>&)"
+		)
+			<< "duplicate data!" << endl
+			<< " index = " << theIndex << endl
+			<< abort(FatalError);
+	}
+}
+
+Standard_Integer 
+tnbLib::SectPx_Registry::Import
+(
+	const std::shared_ptr<SectPx_RegObj>& theObj
+)
+{
+	const auto id = Counter().RetrieveIndex();
+
+	theObj->SetName(theObj->RegObjTypeName() + " nb. " + std::to_string(id));
+	theObj->SetIndex(id);
+
+	Add(id, theObj);
+	return id;
+}
+
+Standard_Integer 
+tnbLib::SectPx_Registry::Import
+(
+	std::shared_ptr<SectPx_RegObj>&& theObj
+)
+{
+	const auto id = Counter().RetrieveIndex();
+
+	theObj->SetName(theObj->RegObjTypeName() + " nb. " + std::to_string(id));
+	theObj->SetIndex(id);
+
+	Add(id, std::move(theObj));
+	return id;
+}
+
+std::shared_ptr<tnbLib::SectPx_RegObj> 
+tnbLib::SectPx_Registry::Remove
+(
+	const Standard_Integer theIndex
+)
+{
+	auto item = RemoveFromMap(theIndex);
+
+	Counter().ReturnToCounter(theIndex);
+	return std::move(item);
+}
+
+void tnbLib::SectPx_Registry::Remove
+(
+	const std::shared_ptr<SectPx_RegObj>& theObj
+)
+{
+#ifdef _DEBUG
+	auto selected = SelectObj(theObj->Index());
+	if (selected NOT_EQUAL theObj)
+	{
+		FatalErrorIn
+		(
+			"void Remove(const std::shared_ptr<SectPx_RegObj>&)"
+		)
+			<< "the removing item is not in the tree!" << endl
+			<< " index = " << theObj->Index() << endl
+			<< abort(FatalError);
+	}
+#endif // _DEBUG
+
+	auto item = RemoveFromMap(theObj->Index());
+	if (item NOT_EQUAL theObj)
+	{
+		Add(item->Index(), std::move(item));
+		FatalErrorIn
+		(
+			"void Remove(const std::shared_ptr<SectPx_Phi>& thePar)"
+		)
+			<< "the removing item is not in the tree!" << endl
+			<< " index = " << theObj->Index() << endl
+			<< abort(FatalError);
+	}
+
+	Counter().ReturnToCounter(theObj->Index());
+}
+
+std::shared_ptr<tnbLib::SectPx_RegObj> 
+tnbLib::SectPx_Registry::SelectObj
+(
+	const Standard_Integer theIndex
+) const
+{
+	auto iter = theObjects_.find(theIndex);
+	if (iter IS_EQUAL theObjects_.end())
+	{
+		FatalErrorIn
+		(
+			"std::shared_ptr<SectPx_RegObj> Select(const Standard_Integer theIndex) const"
+		)
+			<< "the item is not in the tree!" << endl
+			<< " index = " << theIndex << endl
+			<< abort(FatalError);
+	}
+	return iter->second;
+}
+
+std::shared_ptr<tnbLib::SectPx_RegObj> 
+tnbLib::SectPx_Registry::Find
+(
+	const Standard_Integer theIndex
+) const
+{
+	auto iter = theObjects_.find(theIndex);
+	if (iter IS_EQUAL theObjects_.end())
+	{
+		return nullptr;
+	}
+	return iter->second;
+}
+
+void tnbLib::SectPx_Registry::Print
+(
+	Ostream & out
+) const
+{
+	for (const auto& x : theObjects_)
+	{
+		Debug_Null_Pointer(x.second);
+
+		const auto& item = x.second;
+		out << "- index: " << item->Index()
+			<< ", " << getRegObjTypeName(item->RegObjType()) 
+			<< "'s name: " 
+			<< "[" 
+			<< item->Name()
+			<< "]"
+			<< endl;
+	}
+}
+
+std::shared_ptr<tnbLib::SectPx_Registry> 
+tnbLib::sectPxLib::objRegistry = std::make_shared<tnbLib::SectPx_Registry>(0, "registry");
---
+++ TnbSectPx/TnbLib/SectPx/Registry/SectPx_Registry.hxx
@@ -0,0 +1,145 @@
+#pragma once
+#ifndef _SectPx_Registry_Header
+#define _SectPx_Registry_Header
+
+#include <SectPx_Entity.hxx>
+#include <Geo_ItemCounter.hxx>
+#include <SectPx_RegObjType.hxx>
+#include <Ostream.hxx>
+
+#include <map>
+
+namespace tnbLib
+{
+
+	// Forward Declarations
+	class SectPx_RegObj;
+	class SectPx_Par;
+	class SectPx_ParMaker;
+	class SectPx_PntMaker;
+	class SectPx_ProfileMaker;
+
+	class SectPx_Registry
+		: public SectPx_Entity
+	{
+
+		friend class SectPx_ParMaker;
+		friend class SectPx_PntMaker;
+		friend class SectPx_ProfileMaker;
+
+		/*Private Data*/
+
+		std::map<Standard_Integer, std::shared_ptr<SectPx_RegObj>>
+			theObjects_;
+
+		mutable Geo_ItemCounter theCounter_;
+
+
+		//- private functions and operators
+
+		auto& Counter() const
+		{
+			return theCounter_;
+		}
+
+		std::shared_ptr<SectPx_RegObj> 
+			RemoveFromMap
+			(
+				const Standard_Integer theIndex
+			);
+
+		void Add
+		(
+			const Standard_Integer theIndex,
+			const std::shared_ptr<SectPx_RegObj>& theObj
+		);
+
+		void Add
+		(
+			const Standard_Integer theIndex, 
+			std::shared_ptr<SectPx_RegObj>&& theObj
+		);
+
+	protected:
+
+		
+
+	public:
+
+		template<class... _Types>
+		SectPx_Registry(_Types&&... _Args)
+			: SectPx_Entity(_Args...)
+		{}
+
+		auto Size() const
+		{
+			return (Standard_Integer)theObjects_.size();
+		}
+
+		Standard_Boolean 
+			IsContains
+			(
+				const std::shared_ptr<SectPx_RegObj>& theItem
+			) const;
+
+		Standard_Integer
+			Import
+			(
+				const std::shared_ptr<SectPx_RegObj>& theObj
+			);
+
+		Standard_Integer
+			Import
+			(
+				std::shared_ptr<SectPx_RegObj>&& theObj
+			);
+
+		std::shared_ptr<SectPx_RegObj>
+			Remove
+			(
+				const Standard_Integer theIndex
+			);
+
+		//- Throw an exception if not found the item
+		std::shared_ptr<SectPx_RegObj>
+			SelectObj
+			(
+				const Standard_Integer theIndex
+			) const;
+
+		std::shared_ptr<SectPx_RegObj>
+			Find
+			(
+				const Standard_Integer theIndex
+			) const;
+
+		const auto& Objects() const
+		{
+			return theObjects_;
+		}
+
+		void Remove
+		(
+			const std::shared_ptr<SectPx_RegObj>& theObj
+		);
+
+		void Print(Ostream& out) const;
+
+		Standard_Boolean 
+			Next
+			(
+				typename std::map<Standard_Integer, std::shared_ptr<SectPx_RegObj>>::iterator& it, 
+				const sectPxLib::regObjType t
+			) const;
+	};
+
+	namespace sectPxLib
+	{
+		extern std::shared_ptr<SectPx_Registry> objRegistry;
+	}
+}
+
+#include <SectPx_RegistryI.hxx>
+#include <SectPx_RegistryM.hxx>
+
+#endif // !_SectPx_Registry_Header
