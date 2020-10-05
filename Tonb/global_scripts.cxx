79b27b05a2c74bbedb58986a9dbdc8d31e06ce43
aasoleimani <aasoleimani86@gmail.com>
2020-10-05 11:23:30 -0700

    adding 'fileName' and 'OFstream' to the script

--- Tonb/global_scripts.cxx
+++ Tonb/global_scripts.cxx
@@ -1,6 +1,8 @@
 #include "global_scripts.hxx"
 
 #include <word.hxx>
+#include <fileName.hxx>
+#include <OFstream.hxx>
 
 #include <chaiscript/chaiscript.hpp>
 
@@ -16,6 +18,19 @@ void tnbLib::global_scripts(chaiscript::ChaiScript & chai)
 
 	mod->add(chaiscript::fun(static_cast<void (word::*)(const char*)>(&word::operator=)), "=");
 	mod->add(chaiscript::fun(static_cast<void (word::*)(const std::string&)>(&word::operator=)), "=");
+
+	mod->add(chaiscript::user_type<fileName>(), "fileName");
+	mod->add(chaiscript::constructor<fileName()>(), "fileName");
+	mod->add(chaiscript::constructor<fileName(const word&)>(), "fileName");
+	mod->add(chaiscript::constructor<fileName(const std::string&)>(), "fileName");
+	mod->add(chaiscript::constructor<fileName(const char*)>(), "fileName");
+
+	mod->add(chaiscript::user_type<OFstream>(), "ofstream");
+	mod->add(chaiscript::constructor<OFstream(const fileName&)>(), "ofstream");
+	mod->add(chaiscript::constructor<OFstream(const fileName&, IOstream::streamFormat)>(), "ofstream");
+	mod->add(chaiscript::constructor<OFstream(const fileName&, IOstream::streamFormat, IOstream::versionNumber)>(), "ofstream");
+	mod->add(chaiscript::constructor<OFstream(const fileName&, IOstream::streamFormat, IOstream::versionNumber, IOstream::compressionType)>(), "ofstream");
+	mod->add(chaiscript::constructor<OFstream(const fileName&, IOstream::streamFormat, IOstream::versionNumber, IOstream::compressionType, const bool)>(), "ofstream");
 
 	chai.add(mod);
 }
