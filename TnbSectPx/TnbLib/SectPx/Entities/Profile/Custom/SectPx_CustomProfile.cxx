9484e63e4667ab55237be356f5f214d37e4aecb9
aasoleimani <aasoleimani86@gmail.com>
2020-11-09 00:57:04 -0800

    modifying the makeProfile method in the customProfile structure in such a way that edges are created along side of the nodes

--- TnbSectPx/TnbLib/SectPx/Entities/Profile/Custom/SectPx_CustomProfile.cxx
+++ TnbSectPx/TnbLib/SectPx/Entities/Profile/Custom/SectPx_CustomProfile.cxx
@@ -1,8 +1,14 @@
 #include <SectPx_CustomProfile.hxx>
 
+#include <SectPx_Registry.hxx>
+#include <SectPx_Edge.hxx>
+#include <SectPx_TPnt.hxx>
 #include <SectPx_Node.hxx>
+#include <SectPx_NodeMaker.hxx>
 #include <SectPx_InterfaceMaker.hxx>
 #include <SectPx_UniKnots.hxx>
+#include <SectPx_PntTools.hxx>
+#include <SectPx_Tools.hxx>
 #include <TnbError.hxx>
 #include <OSstream.hxx>
 
@@ -50,22 +56,41 @@ tnbLib::SectPx_CustomProfile::RegObjTypeName() const
 std::shared_ptr<tnbLib::SectPx_CustomProfile> 
 tnbLib::SectPx_CustomProfile::MakeProfile
 (
-	const std::shared_ptr<SectPx_Node>& theNode0, 
-	const std::shared_ptr<SectPx_Node>& theNode1,
+	const std::shared_ptr<SectPx_Pnt>& theP0,
+	const std::shared_ptr<SectPx_Pnt>& theP1,
 	const std::shared_ptr<SectPx_Registry>& theReg
 )
 {
-	auto profile = std::make_shared<SectPx_CustomProfile>(theNode0, theNode1);
+	std::vector<std::shared_ptr<SectPx_Pnt>> pnts;
+	pnts.reserve(2);
+	pnts.push_back(theP0);
+	pnts.push_back(theP1);
+
+	auto seg = SectPx_Tools::MakeEdge(theP0, theP1, theReg);
+
+	auto nodeMaker = std::make_shared<maker::Node>(theReg);
+	Debug_Null_Pointer(nodeMaker);
+
+	const auto n0_id = nodeMaker->CreateNode(theP0);
+	const auto n1_id = nodeMaker->CreateNode(theP1);
+
+	const auto n0 = nodeMaker->SelectNode(n0_id);
+	Debug_Null_Pointer(n0);
+
+	const auto n1 = nodeMaker->SelectNode(n1_id);
+	Debug_Null_Pointer(n1);
+
+	auto profile = std::make_shared<SectPx_CustomProfile>(n0, n1);
 	Debug_Null_Pointer(profile);
 
-	SetProfile(theNode0, std::dynamic_pointer_cast<SectPx_TopoProfile>(profile->This()));
-	SetProfile(theNode1, std::dynamic_pointer_cast<SectPx_TopoProfile>(profile->This()));
+	SetProfile(n0, std::dynamic_pointer_cast<SectPx_TopoProfile>(profile->This()));
+	SetProfile(n1, std::dynamic_pointer_cast<SectPx_TopoProfile>(profile->This()));
 
 	const auto interfaceMaker = std::make_shared<maker::Interface>(theReg);
 	Debug_Null_Pointer(interfaceMaker);
 
-	/*const auto intf0_id = */interfaceMaker->CreateEmpty(theNode0);
-	/*const auto intf1_id = */interfaceMaker->CreateEmpty(theNode1);
+	/*const auto intf0_id = */interfaceMaker->CreateEmpty(n0);
+	/*const auto intf1_id = */interfaceMaker->CreateEmpty(n1);
 
 	/*const auto intf0 = interfaceMaker->SelectInterface(intf0_id);
 	Debug_Null_Pointer(intf0);
@@ -76,10 +101,13 @@ tnbLib::SectPx_CustomProfile::MakeProfile
 	/*theNode0->SetInterface(intf0);
 	theNode1->SetInterface(intf1);*/
 
-	auto knotAlg = std::make_shared<SectPx_UniKnots>();
+	// remove this stupid knotAlg from profile structure
+	/*auto knotAlg = std::make_shared<SectPx_UniKnots>();
 	Debug_Null_Pointer(knotAlg);
 
-	profile->SetKnot(std::move(knotAlg));
+	profile->SetKnot(std::move(knotAlg));*/
+
+
 
 	return std::move(profile);
 }
