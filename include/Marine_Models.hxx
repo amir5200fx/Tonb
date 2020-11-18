7f89151b777b5259185f4db8cf600ad2f86ab727
aasoleimani <aasoleimani86@gmail.com>
2020-11-18 13:07:11 -0800

    updating the solution

--- include/Marine_Models.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef _Marine_Models_Header
-#define _Marine_Models_Header
-
-#include <Marine_Model_Hull.hxx>
-#include <Marine_Model_Tank.hxx>
-#include <Marine_SailModels.hxx>
-
-#endif // !_Marine_Models_Header
--- include/SectPx_Datum_CompntI.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace tnbLib
-{
-	inline Pnt2d 
-		sectPxLib::Datum_Compnt::Coord() const
-	{
-		return Pnt2d(xCoord(), yCoord());
-	}
-}
