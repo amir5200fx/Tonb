93308eb878658c9039cf7e1225d920ea6dcfd558
aasoleimani <aasoleimani86@gmail.com>
2020-11-22 06:53:50 -0800

    updating the tnbSectPx lib

---
+++ TnbSectPx/TnbLib/SectPx/Makers/Shape/SectPx_ShapeMaker.cxx
@@ -0,0 +1,20 @@
+#include <SectPx_ShapeMaker.hxx>
+
+#include <SectPx_Poles.hxx>
+
+//void tnbLib::SectPx_ShapeMaker::JoinSegment
+//(
+//	const std::shared_ptr<SectPx_Pole>& thePole, 
+//	const std::shared_ptr<SectPx_Segment>& theSegment,
+//	const short id
+//)
+//{
+//	if (thePole->IsBoundary())
+//	{
+//
+//	}
+//	else if (thePole->IsInterior())
+//	{
+//		
+//	}
+//}
