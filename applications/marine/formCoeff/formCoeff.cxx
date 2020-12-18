7a631ea8fa3a5f32ab967f0abcaccd139d016801
Payvand <peivandhabibi@aut.ac.ir>
2020-12-18 17:37:24 +0330

    Scripts has been added.

--- applications/marine/formCoeff/formCoeff.cxx
+++ applications/marine/formCoeff/formCoeff.cxx
@@ -22,7 +22,7 @@ namespace tnbLib
 	static hModel_t myModel;
 	static std::shared_ptr<formCoeff::Wetted> formCoeffCalculator;
 
-	void loadModdel(const std::string& name)
+	void loadModel(const std::string& name)
 	{
 		fileName fn(name);
 		std::ifstream myFile(fn);
@@ -43,5 +43,88 @@ namespace tnbLib
 		const auto& wettedBody = myModel->FloatBody()->Wetted();
 		formCoeffCalculator = std::make_shared<formCoeff::Wetted>(wettedBody);
 		formCoeffCalculator->Perform();
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+	
+	void formCoefficient(const module_t& mod)
+	{
+
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadModel(name); }), "loadModel");
+		mod->add(chaiscript::fun([]()->void {calcFormCoeff(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << "this is help" << endl;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+
+
+			chai.add(mod);
+
+			fileName myFileName("FormCoefficients");
+
+			try
+			{
+				chai.eval_file(myFileName);
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
 	}
 }
