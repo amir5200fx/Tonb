80aa56ae5483ab5310b3571f6c4be0fc27ccebce
aasoleimani <aasoleimani86@gmail.com>
2020-12-28 20:43:06 -0800

    removing the pln-curve from the project

--- TnbMarineBase/TnbLib/MarineBase/Entities/PlnCurve/HullCurve/Wetted/Marine_WettedCurveIO.cxx
+++
@@ -1,3 +0,0 @@
-#include <Marine_WettedCurve.hxx>
-
-BOOST_CLASS_EXPORT_IMPLEMENT(tnbLib::Marine_WettedCurve);
--- TnbMarineBase/TnbLib/MarineBase/Entities/PlnCurve/WaterCurve/Marine_WaterCurveIO.cxx
+++
@@ -1,3 +0,0 @@
-#include <Marine_WaterCurve.hxx>
-
-BOOST_CLASS_EXPORT_IMPLEMENT(tnbLib::Marine_WaterCurve);
