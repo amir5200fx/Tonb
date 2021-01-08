8ed6b56bcff018315ba070bd3727e1d338e59915
aasoleimani <aasoleimani86@gmail.com>
2021-01-08 00:35:48 -0800

    adding the mesh_node structures

---
+++ TnbMesh/TnbLib/Base/Entities/Node/Adaptor/Mesh_NodeAdaptor.hxx
@@ -0,0 +1,140 @@
+#pragma once
+#ifndef _Mesh_NodeAdaptor_Header
+#define _Mesh_NodeAdaptor_Header
+
+#include <Mesh_EntityAdaptorM.hxx>
+#include <Mesh_Module.hxx>
+
+#define MESH_MODULE TnbMesh_EXPORT
+
+namespace tnbLib
+{
+
+	Mesh_EntityToEntityAdaptor(Node, BoundaryEdge, EdgeType);
+	Mesh_EntityToEntityAdaptor(Node, BoundaryFacet, FacetType);
+
+	Mesh_EntityToEntityAdaptor(Node, Edge, EdgeType);
+	Mesh_EntityToEntityAdaptor(Node, Facet, FacetType);
+	Mesh_EntityToEntityAdaptor(Node, Element, ElementType);
+
+	template<class EdgeType, class ElementType, class FacetType = void>
+	class Mesh_NodeAdaptor
+		: public Mesh_NodeToEdgeAdaptor<EdgeType>
+		, public Mesh_NodeToFacetAdaptor<FacetType>
+		, public Mesh_NodeToElementAdaptor<ElementType>
+	{
+
+		/*Private Data*/
+
+
+		//- private functions and operators
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int /*file_version*/)
+		{
+			ar & boost::serialization::base_object<Mesh_NodeToEdgeAdaptor<EdgeType>>(*this);
+			ar & boost::serialization::base_object<Mesh_NodeToFacetAdaptor<FacetType>>(*this);
+			ar & boost::serialization::base_object<Mesh_NodeToElementAdaptor<ElementType>>(*this);
+		}
+
+	public:
+
+		typedef Mesh_NodeToEdgeAdaptor<EdgeType> edgeAdaptor;
+		typedef Mesh_NodeToFacetAdaptor<FacetType> facetAdaptor;
+		typedef Mesh_NodeToElementAdaptor<ElementType> elementAdaptor;
+
+		Mesh_NodeAdaptor()
+		{}
+	};
+
+	template<class EdgeType, class FacetType>
+	class Mesh_NodeAdaptor<EdgeType, void, FacetType>
+		: public Mesh_NodeToBoundaryEdgeAdaptor<EdgeType>
+		, public Mesh_NodeToBoundaryFacetAdaptor<FacetType>
+	{
+
+		/*Private Data*/
+
+
+
+		//- private functions and operators
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int /*file_version*/)
+		{
+			ar & boost::serialization::base_object<Mesh_NodeToBoundaryEdgeAdaptor<EdgeType>>(*this);
+			ar & boost::serialization::base_object<Mesh_NodeToBoundaryFacetAdaptor<FacetType>>(*this);
+		}
+
+	public:
+
+		typedef Mesh_NodeToBoundaryEdgeAdaptor<EdgeType> edgeAdaptor;
+		typedef Mesh_NodeToBoundaryFacetAdaptor<FacetType> facetAdaptor;
+
+		Mesh_NodeAdaptor()
+		{}
+	};
+
+	template<class EdgeType, class ElementType>
+	class Mesh_NodeAdaptor<EdgeType, ElementType, void>
+		: public Mesh_NodeToEdgeAdaptor<EdgeType>
+		, public Mesh_NodeToElementAdaptor<ElementType>
+	{
+
+		/*Private Data*/
+
+
+		//- private functions and operators
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int /*file_version*/)
+		{
+			ar & boost::serialization::base_object<Mesh_NodeToEdgeAdaptor<EdgeType>>(*this);
+			ar & boost::serialization::base_object<Mesh_NodeToElementAdaptor<ElementType>>(*this);
+		}
+
+	public:
+
+		typedef Mesh_NodeToEdgeAdaptor<EdgeType> edgeAdaptor;
+		typedef Mesh_NodeToElementAdaptor<ElementType> elementAdaptor;
+
+		Mesh_NodeAdaptor()
+		{}
+	};
+
+	template<class EdgeType>
+	class Mesh_NodeAdaptor<EdgeType, void, void>
+		: public Mesh_NodeToBoundaryEdgeAdaptor<EdgeType>
+	{
+
+		/*Private Data*/
+
+
+		//- private functions and operators
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int /*file_version*/)
+		{
+			ar & boost::serialization::base_object<Mesh_NodeToBoundaryEdgeAdaptor<EdgeType>>(*this);
+		}
+
+	public:
+
+		typedef Mesh_NodeToBoundaryEdgeAdaptor<EdgeType> edgeAdaptor;
+
+		Mesh_NodeAdaptor()
+		{}
+	};
+}
+
+#undef MESH_MODULE
+
+#endif // !_Mesh_NodeAdaptor_Header
---
+++ TnbMesh/TnbLib/Base/Entities/Node/Mesh_Node.hxx
@@ -0,0 +1,107 @@
+#pragma once
+#ifndef _Mesh_Node_Header
+#define _Mesh_Node_Header
+
+#include <Mesh_Entity.hxx>
+#include <Mesh_NodeAdaptor.hxx>
+
+#include <memory>
+
+namespace tnbLib
+{
+
+	template<class NodeTraits>
+	class Mesh_Node
+		: public Mesh_Entity
+		, public Mesh_NodeAdaptor
+		<
+		typename NodeTraits::edgeType,
+		typename NodeTraits::elementType,
+		typename NodeTraits::facetType
+		>
+	{
+
+		typedef typename NodeTraits::ptType Point;
+		typedef Mesh_NodeAdaptor
+			<
+			typename NodeTraits::edgeType,
+			typename NodeTraits::elementType,
+			typename NodeTraits::facetType
+			> nodeAdaptor;
+
+		/*Private Data*/
+
+		Point theCoord_;
+
+
+		//- private functions and operators
+
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int /*file_version*/)
+		{
+			ar & boost::serialization::base_object<Mesh_Entity>(*this);
+			ar & boost::serialization::base_object<nodeAdaptor>(*this);
+			ar & theCoord_;
+		}
+
+	public:
+
+		typedef Point ptType;
+
+		enum
+		{
+			rank = 0
+		};
+
+		static const std::shared_ptr<Mesh_Node> null_ptr;
+
+		Mesh_Node()
+		{}
+
+		Mesh_Node
+		(
+			const Standard_Integer theIndex
+		)
+			: Mesh_Entity(theIndex)
+		{}
+
+		Mesh_Node
+		(
+			const Standard_Integer theIndex,
+			const Point& theCoord
+		)
+			: Mesh_Entity(theIndex)
+			, theCoord_(theCoord)
+		{}
+
+		const Point& Coord() const
+		{
+			return theCoord_;
+		}
+
+		Point& Coord()
+		{
+			return theCoord_;
+		}
+
+		void SetCoord(const Point& theCoord)
+		{
+			theCoord_ = theCoord;
+		}
+
+
+		//- Static functions and operators
+
+		static Standard_Boolean IsLess
+		(
+			const std::shared_ptr<Mesh_Node>& theNode0,
+			const std::shared_ptr<Mesh_Node>& theNode1
+		);
+	};
+}
+
+#include <Mesh_NodeI.hxx>
+
+#endif // !_Mesh_Node_Header
