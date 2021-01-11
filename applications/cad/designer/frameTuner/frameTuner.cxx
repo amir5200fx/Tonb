1f91bb7ff87f98f2d7b91a00423a510743a1ddbb
aasoleimani <aasoleimani86@gmail.com>
2021-01-11 11:35:11 -0800

    editing the frameTuner lib

---
+++ applications/cad/designer/frameTuner/frameTuner.cxx
@@ -0,0 +1,30 @@
+#include "frameTuner.hxx"
+
+#include <SectPx_FrameTuner.hxx>
+
+std::tuple<std::pair<int, int>, int> 
+tnbLib::appl::createSlider(const tuner_t & t, int seg, const par_t & p)
+{
+	auto segmt = t->SelectSegment(seg);
+	auto tt = t->CreateSlider(segmt, p);
+	return std::move(tt);
+}
+
+std::tuple<std::pair<int, int>, int> 
+tnbLib::appl::createSlider(const tuner_t & t, int seg, const coord_t & p)
+{
+	auto segmt = t->SelectSegment(seg);
+	auto tt = t->CreateSlider(segmt, p);
+	return std::move(tt);
+}
+
+int tnbLib::appl::createSymmTightness(const tuner_t & t, int i, const par_t & p)
+{
+	auto pole = t->SelectPole(i);
+	return t->CreateSymmTightnessDeg2(pole, p);
+}
+
+void tnbLib::appl::importFrame(const tuner_t & t, const frame_t & f)
+{
+	t->ImportFrame(f);
+}
---
+++ applications/cad/designer/frameTuner/frameTuner.hxx
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef _frameTuner_Header
+#define _frameTuner_Header
+
+#include "frameTunerFwd.hxx"
+#include "frameTuner_Module.hxx"
+
+#include <memory>
+
+namespace tnbLib
+{
+
+	//- Forward Declarations
+
+	namespace appl
+	{
+
+		TnbFrameTunner_EXPORT std::tuple<std::pair<int, int>, int> createSlider(const tuner_t& t, int seg, const par_t& p);
+		TnbFrameTunner_EXPORT std::tuple<std::pair<int, int>, int> createSlider(const tuner_t& t, int seg, const coord_t& p);
+		TnbFrameTunner_EXPORT int createSymmTightness(const tuner_t& t, int pole, const par_t& p);
+
+		TnbFrameTunner_EXPORT void importFrame(const tuner_t& t, const frame_t& f);
+	}
+
+}
+
+#endif // !_frameTuner_Header
