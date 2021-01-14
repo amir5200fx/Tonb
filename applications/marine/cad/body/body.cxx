bdcbf2b199a2814fbf1fe278832187b106f33326
PayvandH <PeivandHabibi@gmail.com>
2021-01-14 20:16:58 +0330

    body application has been updated.

--- applications/marine/cad/body/body.cxx
+++ applications/marine/cad/body/body.cxx
@@ -3,6 +3,7 @@
 #include <Marine_Section.hxx>
 #include <Marine_SectionType.hxx>
 #include <Marine_CmpSection.hxx>
+#include <Marine_Bodies.hxx>
 #include <Pln_Vertex.hxx>
 #include <Pln_Edge.hxx>
 #include <Marine_TankModeler.hxx>
@@ -26,6 +27,9 @@ namespace tnbLib
 	static size_t nbSections = 0;
 
 	static Marine_SectionType bodyType;
+	static std::shared_ptr<Marine_Body> myBody;
+
+	static bool tag = false;
 
 
 	void loadSections(const std::string& name)
@@ -100,5 +104,138 @@ namespace tnbLib
 			}
 		}
 
+		tag = true;
+	}
+
+	void makeBody()
+	{
+		if (!tag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "sections have not been loaded." << endl
+				<< abort(FatalError);
+		}
+
+		switch (bodyType)
+		{
+		case tnbLib::Marine_SectionType::displacer:
+		{
+			myBody = std::make_shared<marineLib::BodyConstructor_noShape<marineLib::Body_Displacer>>(std::move(sections));
+			break;
+		}
+		case tnbLib::Marine_SectionType::sail:
+		{
+			myBody = std::make_shared<marineLib::BodyConstructor_noShape<marineLib::Body_Sail>>(std::move(sections));
+			break;
+		}
+		case tnbLib::Marine_SectionType::tank:
+		{
+			myBody = std::make_shared<marineLib::BodyConstructor_noShape<marineLib::Body_Tank>>(std::move(sections));
+			break;
+		}
+		default:
+			FatalErrorIn(FunctionSIG)
+				<< "This body type is not allowed." << endl
+				<< abort(FatalError);
+		}
+	}
+
+	void saveTo(const std::string& name) 
+	{
+		fileName fn(name);
+		std::ofstream f(fn);
+
+		boost::archive::polymorphic_text_oarchive oa(f);
+
+		oa << myBody;
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void SectionMaker(const module_t& mod)
+	{
+
+		mod->add(chaiscript::fun([](const std::string& name)-> void { loadSections(name); }), "loadSections");
+		mod->add(chaiscript::fun([]()-> void {makeBody(); }), "execute");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << "this is help" << endl;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			SectionMaker(mod);
+
+			chai.add(mod);
+
+			fileName myFileName("body");
+
+			try
+			{
+				chai.eval_file(myFileName);
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
 	}
 }
