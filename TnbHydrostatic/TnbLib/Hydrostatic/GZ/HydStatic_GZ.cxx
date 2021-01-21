704f6393702449c972539343035a3dd2cb65ba68
PayvandH <PeivandHabibi@gmail.com>
2021-01-21 22:58:50 +0330

    updating Gz calculation

--- TnbHydrostatic/TnbLib/Hydrostatic/GZ/HydStatic_GZ.cxx
+++ TnbHydrostatic/TnbLib/Hydrostatic/GZ/HydStatic_GZ.cxx
@@ -6,9 +6,12 @@
 #include <HydStatic_ArmCurveCreator_StbHeel.hxx>
 #include <HydStatic_CrossCurves.hxx>
 #include <HydStatic_CrsCurve.hxx>
+#include <HydStatic_CrsCurvesGraph.hxx>
 #include <HydStatic_CmptLib.hxx>
 #include <TnbError.hxx>
 #include <OSstream.hxx>
+
+bool tnbLib::HydStatic_GZ::verbose = false;
 
 tnbLib::HydStatic_GZ::HydStatic_GZ()
 	: theKg_(marineLib::KG::null)
@@ -72,43 +75,90 @@ namespace tnbLib
 	}
 }
 
-//void tnbLib::HydStatic_GZ::Perform()
-//{
-//	if (NOT CrossCurves())
-//	{
-//		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
-//			<< "no crossCurves is loaded!" << endl
-//			<< abort(FatalError);
-//	}
-//
-//	if (KG().IsNull())
-//	{
-//		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
-//			<< "no KG is specified!" << endl
-//			<< abort(FatalError);
-//	}
-//
-//	if (DISPV().IsNull())
-//	{
-//		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
-//			<< "no DISPV is specified!" << endl
-//			<< abort(FatalError);
-//	}
-//
-//	const auto& crossCurves = *CrossCurves();
-//	const auto t = RetrieveCurveType(crossCurves.CrossCurves());
-//
-//	const auto leverArms = HydStatic_CmptLib::LeverArms(crossCurves.CrossCurves(), DISPV()());
-//	const auto gzQ = HydStatic_CmptLib::GZ(leverArms, KG()());
-//
-//	const auto curve = MarineBase_Tools::Curve(gzQ);
-//
-//	const auto rArm = hydStcLib::MakeCurve<hydStcLib::rArmCurve_Body>(std::move(curve), t);
-//
-//	ChangeRightingArm() = std::move(rArm);
-//
-//	Change_IsDone() = Standard_True;
-//}
+void tnbLib::HydStatic_GZ::Perform()
+{
+	if (verbose)
+	{
+		Info << endl;
+		Info << "******* Calculating GZ Curve ********" << endl;
+		Info << endl;
+	}
+
+	if (NOT CrossCurves())
+	{
+		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
+			<< "no crossCurves is loaded!" << endl
+			<< abort(FatalError);
+	}
+
+	if (KG().IsNull())
+	{
+		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
+			<< "no KG is specified!" << endl
+			<< abort(FatalError);
+	}
+
+	if (DISPV().IsNull())
+	{
+		FatalErrorIn("void tnbLib::HydStatic_GZ::Perform()")
+			<< "no DISPV is specified!" << endl
+			<< abort(FatalError);
+	}
+
+	if (verbose)
+	{
+		Info << " KG: " << KG()() << endl;
+		Info << " Displacement: " << DISPV()() << endl;
+		Info << endl;
+	}
+
+	const auto& crossCurves = *CrossCurves()->CrossCurves();
+	const auto t = RetrieveCurveType(crossCurves.Curves());
+
+	if (NOT INSIDE(DISPV()(), crossCurves.MinDispv(), crossCurves.MaxDispv()))
+	{
+		FatalErrorIn(FunctionSIG)
+			<< "the displacement is not valid for the cross-curves diagram" << endl
+			<< " - min. displacement: " << crossCurves.MinDispv() << endl
+			<< " - max. displacement: " << crossCurves.MaxDispv() << endl
+			<< " - displacement: " << DISPV()() << endl
+			<< abort(FatalError);
+	}
+
+	if (verbose)
+	{
+		Info << " calculating lever arms..." << endl;
+		Info << endl;
+	}
+	const auto leverArms = HydStatic_CmptLib::LeverArms(crossCurves.Curves(), DISPV()());
+
+	if (verbose)
+	{
+		Info << " calculating GZ curve..." << endl;
+		Info << endl;
+	}
+	const auto gzQ = HydStatic_CmptLib::GZ(leverArms, KG()());
+
+	if (verbose)
+	{
+		Info << " calculating geometric curve..." << endl;
+		Info << endl;
+	}
+	const auto curve = MarineBase_Tools::Curve(gzQ);
+
+	const auto rArm = hydStcLib::MakeCurve<hydStcLib::rArmCurve_Body>(std::move(curve), t);
+
+	ChangeRightingArm() = std::move(rArm);
+
+	Change_IsDone() = Standard_True;
+
+	if (verbose)
+	{
+		Info << endl;
+		Info << "******* End of Calculating GZ Curve ********" << endl;
+		Info << endl;
+	}
+}
 
 void tnbLib::HydStatic_GZ::LoadCrossCurves
 (
