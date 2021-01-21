d450c45499f790828b7f643cf79855a1622527cb
PayvandH <PeivandHabibi@gmail.com>
2021-01-21 23:04:50 +0330

    updating the Gz application

--- applications/marine/Gz/Gz.cxx
+++ applications/marine/Gz/Gz.cxx
@@ -1,5 +1,8 @@
+#include <Pnt2d.hxx>
 #include <HydStatic_GZ.hxx>
 #include <HydStatic_CrossCurves.hxx>
+#include <HydStatic_rArmCurve.hxx>
+#include <TecPlot.hxx>
 
 #include <boost/archive/polymorphic_binary_iarchive.hpp>
 #include <boost/archive/polymorphic_binary_oarchive.hpp>
@@ -10,10 +13,40 @@
 #include <algorithm>
 #include <vector>
 
+#include <Geom2d_Curve.hxx>
+
 namespace tnbLib
 {
 
+	static const unsigned int DEFAULT_NB_DIVISIONS_LOW = 15;
+	static const unsigned int DEFAULT_NB_DIVISIONS_MEDIUM = 25;
+	static const unsigned int DEFAULT_NB_DIVISIONS_HIGH = 60;
+
 	static const auto myGz = std::make_shared<HydStatic_GZ>();
+	static bool verbose = false;
+	static auto nPnts = DEFAULT_NB_DIVISIONS_LOW;
+
+	void setTessellation(const word& t)
+	{
+		if (t IS_EQUAL "low")
+		{
+			nPnts = DEFAULT_NB_DIVISIONS_LOW;
+		}
+		else if (t IS_EQUAL "medium")
+		{
+			nPnts = DEFAULT_NB_DIVISIONS_MEDIUM;
+		}
+		else if (t IS_EQUAL "high")
+		{
+			nPnts = DEFAULT_NB_DIVISIONS_HIGH;
+		}
+		else
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "undefined tessellation type" << endl
+				<< abort(FatalError);
+		}
+	}
 
 	void loadCrossCurves(const std::string& name)
 	{
@@ -25,6 +58,19 @@ namespace tnbLib
 		std::shared_ptr<HydStatic_CrossCurves> curves;
 		ar >> curves;
 		myGz->LoadCrossCurves(curves);
+
+		if (verbose)
+		{
+			Info << endl;
+			Info << " the cross-curves are loaded, successfully" << endl;
+			Info << endl;
+		}
+	}
+
+	void setVerbose(bool v)
+	{
+		HydStatic_GZ::verbose = v;
+		verbose = v;
 	}
 
 	void setKG(double x)
@@ -43,6 +89,38 @@ namespace tnbLib
 	{
 		myGz->Perform();
 	}
+
+	auto getPoints(const Handle(Geom2d_Curve)& theCurve)
+	{
+		const auto u1 = theCurve->LastParameter();
+		const auto u0 = theCurve->FirstParameter();
+
+		const auto du = (u1 - u0) / (Standard_Real)(nPnts - 1);
+		std::vector<Pnt2d> pnts;
+		pnts.reserve(nPnts);
+		for (size_t i = 0; i <= nPnts; i++)
+		{
+			auto pt = theCurve->Value(u0 + i * du);
+			pnts.push_back(std::move(pt));
+		}
+		return std::move(pnts);
+	}
+
+	void exportToPlt(const std::string& name)
+	{
+		if (NOT myGz->IsDone())
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the algorithm is not performed" << endl
+				<< abort(FatalError);
+		}
+		fileName fn(name);
+		OFstream f(fn);
+
+		const auto& curve = myGz->RightingArm();
+		auto pnts = getPoints(curve->Geometry());
+		Io::ExportCurve(pnts, f);
+	}
 }
 
 #ifdef DebugInfo
@@ -58,13 +136,16 @@ namespace tnbLib
 
 	void setGlobals(const module_t& mod)
 	{
-
+		mod->add(chaiscript::fun([](bool v)->void {setVerbose(v); }), "setVerbose");
 		mod->add(chaiscript::fun([](double x)->void {setKG(x); }), "setKG");
 		mod->add(chaiscript::fun([](double x)->void {setDispv(x); }), "setDispv");
 		mod->add(chaiscript::fun([]()->void {execute(); }), "execute");
 
+		mod->add(chaiscript::fun([](const std::string& t)-> void {setTessellation(t); }), "setTessellation");
+
 		//- io functions
 
+		mod->add(chaiscript::fun([](const std::string& name)-> void {exportToPlt(name); }), "exportToPlt");
 		mod->add(chaiscript::fun([](const std::string& name)-> void {loadCrossCurves(name); }), "loadCrossCurves");
 
 	}
@@ -107,11 +188,11 @@ int main(int argc, char *argv[])
 
 			auto mod = std::make_shared<chaiscript::Module>();
 
-
+			setGlobals(mod);
 
 			chai.add(mod);
 
-			fileName myFileName("crossCurves");
+			fileName myFileName("Gz");
 
 			try
 			{
@@ -124,6 +205,10 @@ int main(int argc, char *argv[])
 			catch (const error& x)
 			{
 				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
 			}
 		}
 	}
