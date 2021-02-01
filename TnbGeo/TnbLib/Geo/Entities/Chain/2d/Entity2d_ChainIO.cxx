3d2c76f70c1ef9a5c67bb926d8c52434536ece1b
aasoleimani <aasoleimani86@gmail.com>
2021-02-01 09:46:32 -0800

    updating the staticData structure

--- TnbGeo/TnbLib/Geo/Entities/Chain/2d/Entity2d_ChainIO.cxx
+++ TnbGeo/TnbLib/Geo/Entities/Chain/2d/Entity2d_ChainIO.cxx
@@ -1,13 +1,13 @@
-//#include <Entity2d_Chain.hxx>
-//
-//#include <TecPlot.hxx>
-//
-//namespace tnbLib
-//{
-//	
-//	template<>
-//	void Entity2d_Chain::ExportToPlt(OFstream& File) const
-//	{
-//		Io::ExportMesh(thePoints_, theConnectivity_, File);
-//	}
-//}
+#include <Entity2d_Chain.hxx>
+
+#include <TecPlot.hxx>
+
+namespace tnbLib
+{
+	
+	template<>
+	void Entity2d_Chain::ExportToPlt(OFstream& File) const
+	{
+		Io::ExportMesh(thePoints_, theConnectivity_, File);
+	}
+}
