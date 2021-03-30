0dec31311b2f25c2d5b5cd0cfc2e8581e9678b80
aasoleimani <aasoleimani86@gmail.com>
2021-03-30 10:42:10 -0700

    updating the opencascade serialization

--- TnbGlobal/TnbLib/Global/serialization/OpenCascade_Serialization.cxx
+++ TnbGlobal/TnbLib/Global/serialization/OpenCascade_Serialization.cxx
@@ -4,9 +4,12 @@
 #include <GeomTools.hxx>
 #include <istream>
 
+#include <Poly_Triangle.hxx>
 #include <Geom2d_Curve.hxx>
 #include <Geom_Curve.hxx>
 #include <Geom_Surface.hxx>
+#include <TopoDS_Shape.hxx>
+#include <BRepTools.hxx>
 
 template<>
 void boost::serialization::save
@@ -105,4 +108,40 @@ void boost::serialization::load
 	is << st;
 
 	GeomTools::Read(s, is);
+}
+
+template<>
+void boost::serialization::save
+(
+	TNB_oARCH_TYPE & ar,
+	const TopoDS_Shape& sr,
+	const unsigned int version
+)
+{
+	std::stringstream st;
+	st.precision(16);
+	BRepTools::Write(sr, st);
+
+	std::string s = st.str();
+	ar << s;
+}
+
+#include <BRep_Builder.hxx>
+
+template<>
+void boost::serialization::load
+(
+	TNB_iARCH_TYPE & ar,
+	TopoDS_Shape& s,
+	const unsigned int version
+)
+{
+	std::string st;
+	ar >> st;
+
+	std::stringstream is;
+	is << st;
+
+	BRep_Builder b;
+	BRepTools::Read(s, is, b);
 }
--- TnbGlobal/include/OpenCascade_Serialization.hxx
+++ TnbGlobal/include/OpenCascade_Serialization.hxx
@@ -5,10 +5,20 @@
 #include <Standard_Handle.hxx>
 #include <Global_Serialization.hxx>
 
+#include <TColgp_Array1OfPnt.hxx>
+#include <TColgp_Array1OfPnt2d.hxx>
+#include <TShort_Array1OfShortReal.hxx>
+#include <Poly_Array1OfTriangle.hxx>
+
 // Forward Declarations
 class Geom2d_Curve;
 class Geom_Curve;
 class Geom_Surface;
+class TopoDS_Shape;
+class Poly_Triangulation;
+class Poly_Triangle;
+class gp_Pnt;
+class gp_Pnt2d;
 
 namespace boost
 {
@@ -110,11 +120,46 @@ namespace boost
 				Handle(Geom_Surface)& s, 
 				const unsigned int version
 				);
+
+
+		template<class Archive>
+		void save(Archive & /*ar*/, const TopoDS_Shape&, const unsigned int /*version*/)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "this function is supposed to be not calling" << tnbLib::endl
+				<< tnbLib::abort(tnbLib::FatalError);
+		}
+
+		template<class Archive>
+		void load(Archive & /*ar*/, TopoDS_Shape&, const unsigned int /*version*/)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "this function is supposed to be not calling" << tnbLib::endl
+				<< tnbLib::abort(tnbLib::FatalError);
+		}
+
+		template<>
+		TnbGlobal_EXPORT void save<TNB_oARCH_TYPE>
+			(
+				TNB_oARCH_TYPE& ar,
+				const TopoDS_Shape& s,
+				const unsigned int version
+				);
+
+		template<>
+		TnbGlobal_EXPORT void load<TNB_iARCH_TYPE>
+			(
+				TNB_iARCH_TYPE& ar,
+				TopoDS_Shape& s,
+				const unsigned int version
+				);
+
 	}
 }
 
 BOOST_SERIALIZATION_SPLIT_FREE(Handle(Geom2d_Curve))
 BOOST_SERIALIZATION_SPLIT_FREE(Handle(Geom_Curve))
 BOOST_SERIALIZATION_SPLIT_FREE(Handle(Geom_Surface))
+BOOST_SERIALIZATION_SPLIT_FREE(TopoDS_Shape)
 
 #endif // !_OpenCascade_Serialization_Header
