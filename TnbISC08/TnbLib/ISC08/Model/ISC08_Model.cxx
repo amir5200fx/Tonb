da2af97644c1eaa04d2aead0058ceb55e0f8def4
aasoleimani <aasoleimani86@gmail.com>
2021-05-05 22:59:46 -0700

    adding serialization feature into the isc08Model

--- TnbISC08/TnbLib/ISC08/Model/ISC08_Model.cxx
+++ TnbISC08/TnbLib/ISC08/Model/ISC08_Model.cxx
@@ -1,2 +1,57 @@
 #include <ISC08_Model.hxx>
 
+tnbLib::ISC08_Model::ISC08_Model
+(
+	VesselParams && theParameters,
+	Props && theProperties, 
+	const std::shared_ptr<hydStcLib::rArmCurve_Eff>& theGz
+)
+	: theVesselParameters_(std::move(theParameters))
+	, theProperties_(std::move(theProperties))
+	, theEff_(theGz)
+{
+	// empty body
+}
+
+tnbLib::ISC08_Model::ISC08_Model
+(
+	VesselParams && theParameters, 
+	Props && theProperties,
+	std::shared_ptr<hydStcLib::rArmCurve_Eff>&& theGz
+)
+	: theVesselParameters_(std::move(theParameters))
+	, theProperties_(std::move(theProperties))
+	, theEff_(std::move(theGz))
+{
+	// empty body
+}
+
+void tnbLib::ISC08_Model::SetParameters(const VesselParams & theParameters)
+{
+	theVesselParameters_ = theParameters;
+}
+
+void tnbLib::ISC08_Model::SetParameters(VesselParams && theParameters)
+{
+	theVesselParameters_ = std::move(theParameters);
+}
+
+void tnbLib::ISC08_Model::SetProperties(const Props & theProps)
+{
+	theProperties_ = theProps;
+}
+
+void tnbLib::ISC08_Model::SetProperties(Props && theProps)
+{
+	theProperties_ = std::move(theProps);
+}
+
+void tnbLib::ISC08_Model::SetGZeff(const std::shared_ptr<hydStcLib::rArmCurve_Eff>& theGz)
+{
+	theEff_ = theGz;
+}
+
+void tnbLib::ISC08_Model::SetGZeff(std::shared_ptr<hydStcLib::rArmCurve_Eff>&& theGz)
+{
+	theEff_ = std::move(theGz);
+}
