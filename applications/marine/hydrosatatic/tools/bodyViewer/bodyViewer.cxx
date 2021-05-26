114249a5a4d4f37bb1ef7110ee0bae527d484fc0
aasoleimani <aasoleimani86@gmail.com>
2021-05-26 11:14:48 -0700

    retrieving the triangulation from a body

---
+++ applications/marine/hydrosatatic/tools/bodyViewer/bodyViewer.cxx
@@ -0,0 +1,172 @@
+#include <Marine_Bodies.hxx>
+#include <Marine_BodyTools.hxx>
+#include <Entity3d_Chain.hxx>
+#include <Entity3d_Box.hxx>
+#include <Entity3d_Triangulation.hxx>
+#include <Geo_Tools.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<Marine_Body> body_t;
+
+	static unsigned short verbose(0);
+	static body_t myBody;
+	static bool loadTag = false;
+
+	void loadBody(const std::string& name)
+	{
+		fileName fn(name);
+		if (verbose)
+		{
+			Info << endl;
+			Info << " loading the body from, " << fn << endl;
+			Info << endl;
+		}
+		std::ifstream myFile(fn);
+
+		TNB_iARCH_FILE_TYPE ar(myFile);
+		ar >> myBody;
+		if (NOT myBody)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the loaded body is null" << endl
+				<< abort(FatalError);
+		}
+
+		if (verbose)
+		{
+			Info << endl;
+			Info << " the body is loaded, from: " << name << ", successfully!" << endl;
+			Info << endl;
+		}
+
+		loadTag = true;
+	}
+
+	void exportMesh(const std::string& name)
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no body has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		if (NOT myBody)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no body has been found!" << endl
+				<< abort(FatalError);
+		}
+		auto chain = Marine_BodyTools::RetrieveTriangulations(*myBody);
+		auto mesh = Geo_Tools::Triangulation(*chain);
+
+		fileName fn(name);
+		std::ofstream myFile(fn);
+
+		TNB_oARCH_FILE_TYPE ar(myFile);
+		ar << mesh;
+
+		myFile.close();
+
+		if (verbose)
+		{
+			Info << endl;
+			Info << " the body is saved in: " << fn << ", successfully!" << endl;
+			Info << endl;
+		}
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFunctions(const module_t& mod)
+	{
+		mod->add(chaiscript::fun([](unsigned short i)->void {verbose = i; }), "setVerbose");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadBody(name); }), "loadBody");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {exportMesh(name); }), "saveTo");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//sysLib::init_gl_marine_integration_info();
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << "this is help" << endl;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFunctions(mod);
+
+			chai.add(mod);
+
+			fileName myFileName("hydstcBodyViewer");
+
+			try
+			{
+				chai.eval_file(myFileName);
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+}
