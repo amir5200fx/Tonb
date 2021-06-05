acc85c099c238287000cdd91b0a73a0145fb5787
aasoleimani <aasoleimani86@gmail.com>
2021-06-04 23:12:39 -0700

    updating the applications

--- applications/marine/hydrosatatic/hullData/diagrams/bonjean/tools/bonjeanPlotter/bonjeanPlotter.cxx
+++ applications/marine/hydrosatatic/hullData/diagrams/bonjean/tools/bonjeanPlotter/bonjeanPlotter.cxx
@@ -1,27 +1,70 @@
 #include <HydStatic_SolutionData_Bonjean.hxx>
-#include <HydStatic_Bonjean.hxx>
-#include <HydStatic_UniformSpacing.hxx>
-#include <Marine_MultLevWaterDomain.hxx>
-#include <Marine_Bodies.hxx>
-#include <Marine_BodyTools.hxx>
-#include <Marine_WaterLib.hxx>
-#include <Geo_CosineDistb.hxx>
+#include <Marine_Graph.hxx>
+#include <Marine_GraphCurve.hxx>
+#include <Pln_Tools.hxx>
+#include <Geo2d_ApprxCurve.hxx>
+#include <Geo_ApprxCurve_Info.hxx>
+#include <Geo_Tools.hxx>
+#include <Geo_BoxTools.hxx>
+#include <Entity2d_Box.hxx>
+#include <Entity2d_Chain.hxx>
+#include <Entity2d_Triangulation.hxx>
 #include <Entity3d_Box.hxx>
 #include <TnbError.hxx>
 #include <OSstream.hxx>
+
+#include <Geom2d_Curve.hxx>
+#include <Bnd_Box2d.hxx>
 
 namespace tnbLib
 {
 
 	typedef std::shared_ptr<hydStcLib::SolutionData_Bonjean> soluData_t;
-	typedef std::shared_ptr<Marine_MultLevWaterDomain> domains_t;
+	typedef std::shared_ptr<Geo_ApprxCurve_Info> info_t;
+
+	soluData_t mySolutionData;
+	static auto myInfo = std::make_shared<Geo_ApprxCurve_Info>();
 
 	static unsigned short verbose(0);
 
-	static size_t nbWaters = 20;
-	static double offsetD = 0.005;
+	class InfoRunTime
+	{
+	public:
 
-	soluData_t mySolutionData;
+		InfoRunTime()
+		{
+			config();
+		}
+
+		static void config();
+	};
+
+	void InfoRunTime::config()
+	{
+		myInfo->SetAngle(Geo_Tools::DegToRadian(2.0));
+		myInfo->SetApprox(1.0E-3);
+		myInfo->SetInitNbSubdivision(10);
+		myInfo->SetMaxNbSubdivision(20);
+		myInfo->SetMinSize(1.0E-4);
+		myInfo->SetNbSamples(6);
+	}
+
+	static InfoRunTime myInfoRunTime;
+
+	void setAngle(double x)
+	{
+		myInfo->SetAngle(Geo_Tools::DegToRadian(x));
+	}
+
+	void setApprox(double x)
+	{
+		myInfo->SetApprox(x);
+	}
+
+	void setNbSamples(int n)
+	{
+		myInfo->SetNbSamples(n);
+	}
 
 	void loadSolutionDate(const std::string& name)
 	{
@@ -57,83 +100,63 @@ namespace tnbLib
 		if (NOT mySolutionData)
 		{
 			FatalErrorIn(FunctionSIG)
-				<< " no solution data has been loaded!" << endl
+				<< "no solution data has been loaded!" << endl
 				<< abort(FatalError);
 		}
 
+		const auto& graph = mySolutionData->Graph();
+		if (NOT graph)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no bonjean graph has been found!" << endl
+				<< abort(FatalError);
+		}
+
+		Entity2d_Box box;
+		for (const auto& x : graph->Curves())
+		{
+			auto b = Pln_Tools::BoundingBox(Pln_Tools::BoundingBox(x->Curve()));
+			box = Geo_BoxTools::Union(b, box);
+		}
+
+		myInfo->SetApprox(box.Diameter()*myInfo->Approx());
+		myInfo->SetMinSize(box.Diameter()*myInfo->MinSize());
+
+		auto tri = std::make_shared<Entity2d_Chain>();
+		for (const auto& x : graph->Curves())
+		{
+			const auto& geom = x->Curve();
+			auto t = std::make_shared<Geo2d_ApprxCurve>(geom, geom->FirstParameter(), geom->LastParameter(), myInfo);
+			t->Perform();
+
+			const auto& poly = t->Chain();
+			auto& pts = poly->Points();
+
+			auto chain = std::make_shared<Entity2d_Chain>();
+			chain->Points() = std::move(pts);
+			chain->Connectivity() = dualConnectivityList_Chain(chain->NbPoints() - 1);
+
+			auto& chainRef = *chain;
+			tri->Add(std::move(chainRef));
+		}
+
+		auto mesh = Geo_Tools::Triangulation(*tri);
+		
 		fileName fn(name);
 		std::ofstream myFile(fn);
 
+		
+
 		TNB_oARCH_FILE_TYPE ar(myFile);
-		ar << mySolutionData;
-
-		myFile.close();
+		ar << mesh;
 
 		if (verbose)
 		{
 			Info << endl;
-			Info << " the solution data is saved in: " << fn << ", successfully!" << endl;
+			Info << " all curves are saved in: " << fn << ", successfully!" << endl;
 			Info << endl;
 		}
 	}
-
-	auto calcZs(const double z0, const double z1, const int n, const double d)
-	{
-		const auto c = (z1 - z0) / 2;
-		const auto dt = M_PI / (double)n;
-		const auto dz = c * ((double)1.0 - std::cos(dt));
-
-		auto disZ = std::make_shared<Geo_CosineDistb>(n);
-
-		disZ->SetLower(z0 + d * dz);
-		disZ->SetUpper(z1 + d * dz);
-		disZ->Perform();
-
-		return std::move(disZ);
-	}
-
-	void execute()
-	{
-		if (NOT mySolutionData)
-		{
-			FatalErrorIn(FunctionSIG)
-				<< " no solution data has been loaded!" << endl
-				<< abort(FatalError);
-		}
-		if (nbWaters < 3)
-		{
-			FatalErrorIn(FunctionSIG)
-				<< "invalid nb. of waters has been detected!" << endl
-				<< abort(FatalError);
-		}
-		const auto& domain = mySolutionData->Domain();
-		const auto& body = mySolutionData->Body();
-		if (NOT body)
-		{
-			FatalErrorIn(FunctionSIG)
-				<< "no body is found!" << endl
-				<< abort(FatalError);
-		}
-		auto b = Marine_BodyTools::BoundingBox(*body);
-
-		auto Z = calcZs(b.P0().Z(), b.P1().Z(), nbWaters, offsetD);
-		auto domains = Marine_WaterLib::MultiLevelsStillWaterDomain(*body, domain, *Z);
-
-		auto bonj = std::make_shared<HydStatic_Bonjean>();
-		bonj->LoadWaters(domains);
-		bonj->Perform();
-
-		mySolutionData->SetGraph(bonj->Graph());
-
-		if (verbose)
-		{
-			Info << endl;
-			Info << " Bonjean-Curves are calculated, successfully!" << endl;
-			Info << endl;
-		}
-	}
-
-
 }
 
 #ifdef DebugInfo
@@ -149,16 +172,15 @@ namespace tnbLib
 
 	void setGlobals(const module_t& mod)
 	{
-		mod->add(chaiscript::fun([]()->void {execute(); }), "execute");
-		mod->add(chaiscript::fun([](int n)-> void {nbWaters = n; }), "setNbWaters");
-		mod->add(chaiscript::fun([](double d)-> void {offsetD = d; }), "setOffsetZ");
 
+		mod->add(chaiscript::fun([](double x)-> void {setAngle(x); }), "setAngle");
+		mod->add(chaiscript::fun([](double x)-> void {setApprox(x); }), "setApproximate");
+		mod->add(chaiscript::fun([](unsigned int n)-> void {setNbSamples(n); }), "setNbSamples");
 		//- io functions
 
 		mod->add(chaiscript::fun([](const std::string& name)->void {saveTo(name); }), "saveTo");
 		mod->add(chaiscript::fun([](const std::string& name)-> void {loadSolutionDate(name); }), "loadSoluData");
 		mod->add(chaiscript::fun([](unsigned short c)-> void {verbose = c; }), "setVerbose");
-		mod->add(chaiscript::fun([](unsigned short c)-> void {HydStatic_Bonjean::verbose = c; }), "setBonjeanVerbose");
 
 	}
 
@@ -205,7 +227,7 @@ int main(int argc, char *argv[])
 
 			chai.add(mod);
 
-			fileName myFileName("hullDataBonjean");
+			fileName myFileName("hullDataBonjeanPlotter");
 
 			try
 			{
