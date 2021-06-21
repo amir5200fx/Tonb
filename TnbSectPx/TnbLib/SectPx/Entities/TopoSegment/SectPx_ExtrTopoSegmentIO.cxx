749f9d0c0fcd07bb2b3020350262718883702749
aminyaa <ma.yaaghoubi@gmail.com>
2021-06-21 20:49:45 +0430

    add serialization for "SectPx_ExtrTopoSegment" class

--- TnbSectPx/TnbLib/SectPx/Entities/TopoSegment/SectPx_ExtrTopoSegmentIO.cxx
+++ TnbSectPx/TnbLib/SectPx/Entities/TopoSegment/SectPx_ExtrTopoSegmentIO.cxx
@@ -1,3 +1,13 @@
 #include <SectPx_ExtrTopoSegment.hxx>
 
+TNB_SAVE_IMPLEMENTATION(tnbLib::SectPx_ExtrTopoSegment)
+{
+	ar& boost::serialization::base_object<SectPx_TopoSegment>(*this);
+}
+
+TNB_LOAD_IMPLEMENTATION(tnbLib::SectPx_ExtrTopoSegment)
+{
+	ar& boost::serialization::base_object<SectPx_TopoSegment>(*this);
+}
+
 BOOST_CLASS_EXPORT_IMPLEMENT(tnbLib::SectPx_ExtrTopoSegment);
