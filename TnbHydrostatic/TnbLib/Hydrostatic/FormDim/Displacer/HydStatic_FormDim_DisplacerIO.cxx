48e2db9cdf82b4d50b848b5f2393f31d30157626
aasoleimani <aasoleimani86@gmail.com>
2021-07-17 09:50:50 -0700

    debugging the serialization functions

--- TnbHydrostatic/TnbLib/Hydrostatic/FormDim/Displacer/HydStatic_FormDim_DisplacerIO.cxx
+++ TnbHydrostatic/TnbLib/Hydrostatic/FormDim/Displacer/HydStatic_FormDim_DisplacerIO.cxx
@@ -20,7 +20,8 @@ TNB_SAVE_IMPLEMENTATION(tnbLib::formDim::Displacer)
 {
 	ar & boost::serialization::base_object<Global_Done>(*this);
 	Marine_Body::Save(ar, theBody_);
-	//ar & theParameters_;
+	//ar & theBody_;
+	ar & theParameters_;
 }
 
 TNB_LOAD_IMPLEMENTATION(tnbLib::formDim::Displacer)
@@ -29,5 +30,6 @@ TNB_LOAD_IMPLEMENTATION(tnbLib::formDim::Displacer)
 	std::shared_ptr<Marine_Body> b;
 	Marine_Body::Load(ar, b);
 	theBody_ = std::dynamic_pointer_cast<marineLib::Body_Displacer>(b);
-	//ar & theParameters_;
+	//ar & theBody_;
+	ar & theParameters_;
 }
