4d416ac3258159e77a138c1729b0062d92642a60
aasoleimani <aasoleimani86@gmail.com>
2021-07-17 10:03:49 -0700

    corner size map application

---
+++ applications/mesh/sizeMap/2d/corner/cornerSizeMap/cornerSizeMap.cxx
@@ -0,0 +1,14 @@
+#include <Mesh_ReferenceValues.hxx>
+#include <BoundarySizeMap2d_CornerToolSystem.hxx>
+#include <BoundarySizeMap2d_CornerTool.hxx>
+#include <Cad2d_Plane.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+
+	static unsigned short verbose = 0;
+
+	
+}
