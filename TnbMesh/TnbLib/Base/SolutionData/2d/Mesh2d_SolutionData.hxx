37ffc356fe3686f9f56381baa709a7603c80352e
aasoleimani <aasoleimani86@gmail.com>
2021-07-17 09:54:05 -0700

    introducing a solution data

--- TnbMesh/TnbLib/Base/SolutionData/2d/Mesh2d_SolutionData.hxx
+++ TnbMesh/TnbLib/Base/SolutionData/2d/Mesh2d_SolutionData.hxx
@@ -5,6 +5,24 @@
 #include <Mesh_SolutionData.hxx>
 #include <Mesh2d_SolutionDataFwd.hxx>
 
+namespace tnbLib
+{
 
+	template<>
+	template<>
+	TnbMesh_EXPORT void Mesh2d_SolutionData::serialize<TNB_iARCH_TYPE>
+		(
+			TNB_iARCH_TYPE& ar,
+			const unsigned int /*file_version*/
+			);
+
+	template<>
+	template<>
+	TnbMesh_EXPORT void Mesh2d_SolutionData::serialize<TNB_oARCH_TYPE>
+		(
+			TNB_oARCH_TYPE& ar,
+			const unsigned int /*file_version*/
+			);
+}
 
 #endif // !_Mesh2d_SolutionData_Header
