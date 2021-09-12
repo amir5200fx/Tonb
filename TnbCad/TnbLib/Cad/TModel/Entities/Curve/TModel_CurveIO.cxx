a6a63b94a186b0d4d6a9d46e5aa6d900613eb950
aasoleimani <aasoleimani86@gmail.com>
2021-09-12 12:36:18 -0700

    updating io function

--- TnbCad/TnbLib/Cad/TModel/Entities/Curve/TModel_CurveIO.cxx
+++ TnbCad/TnbLib/Cad/TModel/Entities/Curve/TModel_CurveIO.cxx
@@ -6,13 +6,30 @@
 TNB_SAVE_IMPLEMENTATION(tnbLib::TModel_Curve)
 {
 	ar & boost::serialization::base_object<TModel_Entity>(*this);
-	ar & theGeometry_;
+
+	bool hasGeom = false;
+	if (theGeometry_)
+	{
+		hasGeom = true;
+		ar & hasGeom;
+		ar & theGeometry_;
+	}
+	else
+	{
+		ar & hasGeom;
+	}
 }
 
 TNB_LOAD_IMPLEMENTATION(tnbLib::TModel_Curve)
 {
 	ar & boost::serialization::base_object<TModel_Entity>(*this);
-	ar & theGeometry_;
+
+	bool hasGeom;
+	ar& hasGeom;
+	if (hasGeom)
+	{
+		ar & theGeometry_;
+	}
 }
 
 BOOST_CLASS_EXPORT_IMPLEMENT(tnbLib::TModel_Curve);
