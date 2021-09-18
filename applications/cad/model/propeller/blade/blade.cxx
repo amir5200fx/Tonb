2039c3250bab82ce1a3b9b92b289df3e3f067416
aasoleimani <aasoleimani86@gmail.com>
2021-09-17 20:55:34 -0700

    moving the script file into the system subdirectory

--- applications/cad/model/propeller/blade/blade.cxx
+++ applications/cad/model/propeller/blade/blade.cxx
@@ -231,7 +231,8 @@ int main(int argc, char *argv[])
 
 			chai.add(mod);
 
-			fileName myFileName("propBladeMaker");
+			std::string address = ".\\system\\propBladeMaker";
+			fileName myFileName(address);
 
 			try
 			{
