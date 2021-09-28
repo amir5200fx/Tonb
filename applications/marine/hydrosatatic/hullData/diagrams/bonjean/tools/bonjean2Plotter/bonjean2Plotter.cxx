2b16d9d913e73b2636ea889c34793d6cdfd79ac0
aasoleimani <aasoleimani86@gmail.com>
2021-09-28 10:55:58 -0700

    adding bonjean plotter into the solution

---
+++ applications/marine/hydrosatatic/hullData/diagrams/bonjean/tools/bonjean2Plotter/bonjean2Plotter.cxx
@@ -0,0 +1,267 @@
+#include <HydStatic_SolutionData_Bonjean2.hxx>
+#include <HydStatic_Bonjean2.hxx>
+#include <HydStatic_HydOffsetCurve.hxx>
+#include <Entity2d_Chain.hxx>
+#include <Entity2d_Triangulation.hxx>
+#include <Entity2d_Box.hxx>
+#include <Geo_Tools.hxx>
+#include <Global_Timer.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+#include <fstream>
+
+#include <boost/filesystem.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<hydStcLib::SolutionData_Bonjean2> soluData_t;
+
+	static soluData_t mySolutionData;
+	static std::vector<std::shared_ptr<Entity2d_Triangulation>> myTris;
+
+	static bool loadTag = false;
+	static bool exeTag = false;
+	static unsigned short verbose(0);
+
+	void setVerbose(unsigned short i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void loadModel(const std::string& name)
+	{
+		fileName fn(name);
+		if (verbose)
+		{
+			Info << endl;
+			Info << " loading the model from, " << fn << endl;
+			Info << endl;
+		}
+		std::ifstream myFile(fn);
+
+		{//- timer scope
+			Global_Timer timer;
+			timer.SetInfo(Global_TimerInfo_ms);
+
+			TNB_iARCH_FILE_TYPE ar(myFile);
+			ar >> mySolutionData;
+		}
+
+		if (NOT mySolutionData)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the loaded model is null" << endl
+				<< abort(FatalError);
+		}
+
+		if (verbose)
+		{
+			Info << endl;
+			Info << " the model is loaded, from: " << name << ", successfully in " << global_time_duration << " ms." << endl;
+			Info << endl;
+		}
+
+		loadTag = true;
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application has not been performed!" << endl
+				<< abort(FatalError);
+		}
+
+		if (verbose)
+		{
+			Info << endl;
+		}
+
+		size_t i = 0;
+		for (const auto& x : myTris)
+		{
+			std::string address = ".\\" + std::to_string(i) + "\\" + name;
+			boost::filesystem::path dir(std::to_string(i));
+			boost::filesystem::create_directory(dir);
+
+			std::ofstream file(address);
+
+			TNB_oARCH_FILE_TYPE oa(file);
+			oa << x;
+
+			if (verbose)
+			{
+				Info << " - section's curve are saved in: " << address << ", successfully!" << endl;
+			}
+			i++;
+		}
+
+		if (verbose)
+		{
+			Info << endl
+				<< " all section curves are saved, successfully!" << endl;
+		}
+	}
+
+	auto retrieveChain(const std::vector<std::pair<Standard_Real, Standard_Real>>& Qs)
+	{
+		auto chain = std::make_shared<Entity2d_Chain>();
+		auto& pnts = chain->Points();
+		pnts.reserve(Qs.size());
+		for (const auto& x : Qs)
+		{
+			auto pt = Pnt2d(x.first, x.second);
+			pnts.push_back(std::move(pt));
+		}
+		chain->Connectivity() = dualConnectivityList_Chain(chain->NbPoints() - 1);
+		return std::move(chain);
+	}
+
+	void execute()
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no solution data has been loaded, yet!" << endl
+				<< abort(FatalError);
+		}
+
+		const auto& sections = mySolutionData->Sections();
+
+		std::vector<std::shared_ptr<Entity2d_Triangulation>> tris;
+		tris.reserve(sections.size());
+		for (const auto& x : sections)
+		{
+			const auto& offsetCurve = x->Curve;
+			const auto& Qs = offsetCurve->Qs();
+
+			auto chain = retrieveChain(Qs);
+			auto tri = Geo_Tools::Triangulation(*chain);
+			tris.push_back(std::move(tri));
+		}
+		myTris = std::move(tris);
+		exeTag = true;
+
+		if (verbose)
+		{
+			Info << endl
+				<< " the application is performed, successfully!" << endl;
+		}
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFunctions(const module_t& mod)
+	{
+		//- io functions
+		mod->add(chaiscript::fun([](const std::string& name)->void {loadModel(name); }), "loadModel");
+		mod->add(chaiscript::fun([](const std::string& name)->void {saveTo(name); }), "saveTo");
+
+		//- settings
+		mod->add(chaiscript::fun([](unsigned short t)->void {setVerbose(t); }), "setVerbose");
+
+		mod->add(chaiscript::fun([]()->void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << endl;
+			Info << " This application is aimed to retrieve the Bonjean curves from the solution data." << endl;
+			Info << endl
+				<< " Function list:" << endl
+				<< " - loadModel(string)" << endl
+				<< " - saveTo(string)" << endl << endl
+
+				<< " - setVerbose(unsigned int);    - Levels: 0, 1" << endl << endl
+
+				<< " - execute()" << endl
+				<< endl;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFunctions(mod);
+
+			chai.add(mod);
+
+			std::string address = ".\\system\\hullDataBonjean2Plotter";
+			fileName myFileName(address);
+
+			try
+			{
+				chai.eval_file(myFileName);
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+}
