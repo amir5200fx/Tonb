1356b40d052dee18c7c90d837c230ae6db9acbe7
aasoleimani <aasoleimani86@gmail.com>
2021-12-23 05:20:30 -0800

    adding planar mesh viewer

---
+++ applications/mesh/plane/tools/viewer/tnbMeshToolPlaneViewer/tnbMeshToolPlaneViewer.cxx
@@ -0,0 +1,215 @@
+#include <Aft2d_Model.hxx>
+#include <Aft2d_SolutionData.hxx>
+#include <Aft2d_SolutionDataAnIso.hxx>
+#include <Aft2d_Element.hxx>
+#include <Aft2d_ElementAnIso.hxx>
+#include <Aft_Tools.hxx>
+#include <Entity2d_Triangulation.hxx>
+#include <Global_File.hxx>
+#include <OSstream.hxx>
+#include <TnbError.hxx>
+
+namespace tnbLib
+{
+
+	static const std::string extension = Aft2d_SolutionDataBase::extension;
+
+	static unsigned short verbose = 0;
+	static bool loadTag = false;
+
+	static std::shared_ptr<Aft2d_SolutionDataBase> mySoluData;
+	static std::string myFileName;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void loadFile(const std::string& name)
+	{
+		file::CheckExtension(name);
+
+		mySoluData = file::LoadFile<std::shared_ptr<Aft2d_SolutionDataBase>>(name + extension, verbose);
+		if (NOT mySoluData)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the data solution file is null!" << endl
+				<< abort(FatalError);
+		}
+
+		loadTag = true;
+	}
+
+	void loadFile()
+	{
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), extension).string();
+		myFileName = name;
+		loadFile(name);
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no file has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		file::CheckExtension(name);
+
+		if (mySoluData->IsIso())
+		{
+			auto soluData = std::dynamic_pointer_cast<Aft2d_SolutionData>(mySoluData);
+			Debug_Null_Pointer(soluData);
+
+			const auto& elements = soluData->Elements();
+			if (elements.empty())
+			{
+				FatalErrorIn(FunctionSIG)
+					<< "no element has been detected!" << endl
+					<< abort(FatalError);
+			}
+
+			auto mesh = Aft_Tools::RetrieveTriangleMesh(elements);
+
+			file::SaveTo(mesh, name + Entity2d_Triangulation::extension, verbose);
+		}
+		else if (mySoluData->IsAnIso())
+		{
+			NotImplemented;
+		}
+		else
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "Unspecified type of data has been detected!" << endl
+				<< abort(FatalError);
+		}
+	}
+
+	void saveTo()
+	{
+		saveTo(myFileName);
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFuns(const module_t& mod)
+	{
+		// io functions 
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadFile(name); }), "loadFile");
+
+		// settings 
+
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators 
+
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << " This application is aimed to retrieve the boundary mesh." << endl << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int); Levels: 0, 1" << endl << endl
+
+				<< " # Operators:" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFuns(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbMeshToolPlaneViewer"));
+
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
