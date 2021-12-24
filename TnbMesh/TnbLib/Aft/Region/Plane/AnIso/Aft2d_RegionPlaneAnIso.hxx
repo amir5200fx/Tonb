c0326f6b240b40a419abe5531c18eb38153c2318
aasoleimani <aasoleimani86@gmail.com>
2021-12-24 02:00:18 -0800

    updating the serialization

--- TnbMesh/TnbLib/Aft/Region/Plane/AnIso/Aft2d_RegionPlaneAnIso.hxx
+++ TnbMesh/TnbLib/Aft/Region/Plane/AnIso/Aft2d_RegionPlaneAnIso.hxx
@@ -2,7 +2,27 @@
 #ifndef _Aft2d_RegionPlaneAnIso_Header
 #define _Aft2d_RegionPlaneAnIso_Header
 
+#include <Mesh_Module.hxx>
 #include <Mesh_RegionPlane.hxx>
 #include <Aft2d_RegionPlaneAnIsoFwd.hxx>
+
+namespace tnbLib
+{
+	template<>
+	template<>
+	TnbMesh_EXPORT void Aft2d_RegionPlaneAnIso::serialize<TNB_iARCH_TYPE>
+		(
+			TNB_iARCH_TYPE& ar,
+			const unsigned int file_version
+			);
+
+	template<>
+	template<>
+	TnbMesh_EXPORT void Aft2d_RegionPlaneAnIso::serialize<TNB_oARCH_TYPE>
+		(
+			TNB_oARCH_TYPE& ar,
+			const unsigned int file_version
+			);
+}
 
 #endif // !_Aft2d_RegionPlaneAnIso_Header
