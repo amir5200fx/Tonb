bf0b3a29b950f077f74bc0618842db2c2e4dd310
aasoleimani <aasoleimani86@gmail.com>
2021-12-24 02:04:06 -0800

    adding aniso node generator application into the solution

---
+++ applications/mesh/plane/nodeCreator/anIso/std/tnbMeshPlaneAnIsoAftStdNodeCreator/tnbMeshPlaneAnIsoAftStdNodeCreator.cxx
@@ -0,0 +1,188 @@
+#include <Aft2d_StdOptNodeAnIso.hxx>
+#include <Global_File.hxx>
+#include <OSstream.hxx>
+#include <TnbError.hxx>
+
+namespace tnbLib
+{
+
+	static const std::string extension = Aft2d_OptNodeAnIso_Calculator::extension;
+
+	static unsigned short verbose = 0;
+	static bool exeTag = false;
+
+	static std::shared_ptr<Aft2d_OptNodeAnIso_Calculator> myNodeCreator;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+
+		file::CheckExtension(name);
+
+		file::SaveTo(myNodeCreator, name + extension, verbose);
+	}
+
+	void saveTo()
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+
+		saveTo(myNodeCreator->Name());
+	}
+
+	void execute(const std::string& name)
+	{
+		myNodeCreator = std::make_shared<Aft2d_StdOptNodeAnIso>();
+		myNodeCreator->SetName(name);
+
+		exeTag = true;
+
+		if (verbose)
+		{
+			Info << endl
+				<< " the application is performed, successfully!" << endl;
+		}
+	}
+
+	void execute()
+	{
+		execute("stdNodeCreator");
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFunctions(const module_t& mod)
+	{
+		// io functions [12/21/2021 Amir]
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+
+		// settings [12/21/2021 Amir]
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators [12/21/2021 Amir]
+		mod->add(chaiscript::fun([](const std::string& name)->void {execute(name); }), "execute");
+		mod->add(chaiscript::fun([]()->void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		tnbLib::Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << endl;
+			Info << " This application is aimed to create an anisotropic node creator for the advancing front." << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int);    - Levels: 0, 1" << endl << endl
+
+				<< " # operators: " << endl << endl
+
+				<< " - execute(name [optional])" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFunctions(mod);
+
+			chai.add(mod);
+
+
+			try
+			{
+				fileName theFileName(file::GetSystemFile("tnbMeshPlaneAnIsoAftStdNodeCreator"));
+
+				chai.eval_file(theFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				tnbLib::Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				tnbLib::Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				tnbLib::Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			tnbLib::Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		tnbLib::Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
