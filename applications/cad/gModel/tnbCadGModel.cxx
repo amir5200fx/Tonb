1cb032cf5cd699aa027f1313e294e88ea2e38b8b
aasoleimani <aasoleimani86@gmail.com>
2021-12-28 12:25:35 -0800

    adding GModel maker into the solution

---
+++ applications/cad/gModel/tnbCadGModel.cxx
@@ -0,0 +1,236 @@
+#include <Cad3d_GModel.hxx>
+#include <GModel_Tools.hxx>
+#include <Cad_Shape.hxx>
+#include <Global_File.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+
+	static const std::string loadExt = Cad_Shape::extension;
+	static const std::string saveExt = Cad3d_GModel::extension;
+
+	static unsigned short verbose = 0;
+	static bool loadTag = false;
+	static bool exeTag = false;
+
+	static std::shared_ptr<Cad3d_GModel> myModel;
+	static std::shared_ptr<Cad_Shape> myShape;
+	static std::string myFileName;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void loadFile(const std::string& name)
+	{
+		file::CheckExtension(name);
+
+		myShape = file::LoadFile<std::shared_ptr<Cad_Shape>>(name + loadExt, verbose);
+		if (NOT myShape)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the shape file is null!" << endl
+				<< abort(FatalError);
+		}
+		loadTag = true;
+	}
+
+	void loadFile()
+	{
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), loadExt).string();
+		myFileName = name;
+		loadFile(name);
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+
+		file::CheckExtension(name);
+
+		file::SaveTo(myModel, name + saveExt, verbose);
+	}
+
+	void saveTo()
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+		saveTo(myFileName);
+	}
+
+	void execute(const std::string& name)
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no file has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		auto surfaces = GModel_Tools::GetSurfaces(myShape->Shape());
+		if (surfaces.empty())
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no surface is found!" << endl
+				<< abort(FatalError);
+		}
+
+		myModel = std::make_shared<Cad3d_GModel>(std::move(surfaces));
+		myModel->SetName(name);
+
+		if (verbose)
+		{
+			Info << endl
+				<< " - the application is performed, successfully!" << endl;
+		}
+		exeTag = true;
+	}
+
+	void execute()
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no file has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		execute(myShape->Name());
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFuns(const module_t& mod)
+	{
+		// io functions [12/28/2021 Amir]
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadFile(name); }), "loadFile");
+
+		// settings [12/28/2021 Amir]
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators [12/28/2021 Amir]
+		mod->add(chaiscript::fun([]()-> void {execute(); }), "execute");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {execute(name); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << " This application is aimed to create a GModel for a shape." << endl << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int); Levels: 0, 1, 2" << endl << endl
+
+				<< " # Operators:" << endl << endl
+
+				<< " - execute(name [optional])" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFuns(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbCadGModel"));
+
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
