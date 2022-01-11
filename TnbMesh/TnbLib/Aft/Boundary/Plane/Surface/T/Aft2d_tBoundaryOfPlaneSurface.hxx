325a2c46b649b8b89107432cda9b1fb7bc397e88
aasoleimani <aasoleimani86@gmail.com>
2022-01-11 06:16:23 -0800

    updaitng the tnbMesh

---
+++ TnbMesh/TnbLib/Aft/Boundary/Plane/Surface/T/Aft2d_tBoundaryOfPlaneSurface.hxx
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _Aft2d_tBoundaryOfPlaneSurface_Header
+#define _Aft2d_tBoundaryOfPlaneSurface_Header
+
+#include <Geo2d_SizeFunction.hxx>
+#include <Geo2d_MetricFunction.hxx>
+#include <Aft_BoundaryOfPlane.hxx>
+#include <Aft2d_tRegionPlaneSurface.hxx>
+#include <Aft2d_tSegmentEdge.hxx>
+#include <Aft2d_tPlnWireSurface.hxx>
+#include <Aft2d_BoundaryOfPlaneAnIso_Info.hxx>
+#include <Aft2d_tBoundaryOfPlaneSurfaceFwd.hxx>
+
+#endif // !_Aft2d_tBoundaryOfPlaneSurface_Header
