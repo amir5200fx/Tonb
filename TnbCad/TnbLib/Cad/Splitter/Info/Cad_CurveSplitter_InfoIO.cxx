c002fa22b11cf785efb6d6cb3dd275a7f2ee65f3
aasoleimani <aasoleimani86@gmail.com>
2022-01-24 23:06:58 -0800

    curveSplitter lib has been completed

---
+++ TnbCad/TnbLib/Cad/Splitter/Info/Cad_CurveSplitter_InfoIO.cxx
@@ -0,0 +1,35 @@
+#include <Cad_CurveSplitter_Info.hxx>
+
+#include <Mesh_CurveOptmPoint_Correction_Info.hxx>
+#include <NumAlg_AdaptiveInteg_Info.hxx>
+#include <NumAlg_NewtonSolver_Info.hxx>
+
+TNB_SAVE_IMPLEMENTATION(tnbLib::Cad_CurveSplitter_Info)
+{
+	ar & theCorrInfo_;
+	ar & theNewtonInfo_;
+
+	ar & theOverallLengthIntgInfo_;
+	ar & theIterIntgInfo_;
+
+	ar & theMaxLevel_;
+
+	ar & theUnderRelaxation_;
+
+	ar & theIgnorNonConvergency_;
+}
+
+TNB_LOAD_IMPLEMENTATION(tnbLib::Cad_CurveSplitter_Info)
+{
+	ar & theCorrInfo_;
+	ar & theNewtonInfo_;
+
+	ar & theOverallLengthIntgInfo_;
+	ar & theIterIntgInfo_;
+
+	ar & theMaxLevel_;
+
+	ar & theUnderRelaxation_;
+
+	ar & theIgnorNonConvergency_;
+}
