0cb65cc2c3d457f2218d75e8cd8048353d84165c
aasoleimani <aasoleimani86@gmail.com>
2022-02-18 07:23:44 -0800

    adding a function to the geo_tools lib to calculate a planar polygon length.

--- TnbGeo/TnbLib/Geo/Tools/Geo_Tools.cxx
+++ TnbGeo/TnbLib/Geo/Tools/Geo_Tools.cxx
@@ -122,6 +122,24 @@ tnbLib::Geo_Tools::GetIntersectionSegment
 	return std::move(seg);
 }
 
+Standard_Real 
+tnbLib::Geo_Tools::CalcLength
+(
+	const Entity2d_Polygon & thePoly
+)
+{
+	const auto& pts = thePoly.Points();
+	Standard_Real d = 0;
+	for (size_t i = 1; i < pts.size(); i++)
+	{
+		const auto& p0 = pts[i - 1];
+		const auto& p1 = pts[i];
+
+		d += p0.Distance(p1);
+	}
+	return d;
+}
+
 Standard_Boolean
 tnbLib::Geo_Tools::IsCcwOrder
 (
