3e3a69b8de22d2f523b33b02128e1d6680c14b7a
aasoleimani <aasoleimani86@gmail.com>
2022-02-18 07:20:17 -0800

    the Trim method has been disabled.

--- TnbCad2d/TnbLib/Cad2d/Modeler/Cad2d_Modeler.cxx
+++ TnbCad2d/TnbLib/Cad2d/Modeler/Cad2d_Modeler.cxx
@@ -875,105 +875,105 @@ namespace tnbLib
 	typedef std::vector<std::shared_ptr<Cad2d_IntsctEntity_Segment>> entityList;
 }
 
-void tnbLib::Cad2d_Modeler::Trim
-(
-	const std::shared_ptr<Pln_Edge>& theEdge0, 
-	const std::shared_ptr<Pln_Edge>& theEdge1
-)
-{
-	if (NOT theEdge0)
-	{
-		FatalErrorIn("void Trim(const std::shared_ptr<Pln_Edge>& theEdge0, const std::shared_ptr<Pln_Edge>& theEdge1)")
-			<< "the edge0 is null" << endl
-			<< abort(FatalError);
-	}
-
-	if (NOT theEdge1)
-	{
-		FatalErrorIn("void Trim(const std::shared_ptr<Pln_Edge>& theEdge0, const std::shared_ptr<Pln_Edge>& theEdge1)")
-			<< "the edge1 is null" << endl
-			<< abort(FatalError);
-	}
-
-	auto intAlg = std::make_shared<Cad2d_EdgeEdgeIntersection>();
-	Debug_Null_Pointer(intAlg);
-
-	intAlg->LoadEdge0(theEdge0);
-	intAlg->LoadEdge1(theEdge1);
-
-	intAlg->Perform();
-	Debug_If_Condition_Message(NOT intAlg->IsDone(), "the intersection algorithm is not performed!");
-
-	if (NOT intAlg->NbEntities())
-	{
-		return;
-	}
-
-	const auto& pairList = intAlg->Entities();
-
-	entityList entities0, entities1;
-	for (const auto& x : pairList)
-	{
-		Debug_Null_Pointer(x);
-
-		auto ent0 = std::dynamic_pointer_cast<Cad2d_IntsctEntity_Segment>(x->Entity0());
-		Debug_Null_Pointer(ent0);
-
-		auto ent1 = std::dynamic_pointer_cast<Cad2d_IntsctEntity_Segment>(x->Entity1());
-		Debug_Null_Pointer(ent1);
-
-		entities0.push_back(std::move(ent0));
-		entities1.push_back(std::move(ent1));
-	}
-
-	Cad2d_IntsctEntity_Segment::SortEntities(entities0);
-	Cad2d_IntsctEntity_Segment::SortEntities(entities1);
-
-	auto sub0 = Cad2d_IntsctEntity_Segment::SubdivideEdge
-	(
-		theEdge0, entities0, 
-		std::max(theEdge0->Vtx0()->Precision(), theEdge0->Vtx1()->Precision())
-	);
-
-	if (NOT theEdge0->Sense())
-	{
-		std::reverse(sub0.begin(), sub0.end());
-	}
-
-	auto sub1 = Cad2d_IntsctEntity_Segment::SubdivideEdge
-	(
-		theEdge1, entities1,
-		std::max(theEdge1->Vtx0()->Precision(), theEdge1->Vtx1()->Precision())
-	);
-
-	if (NOT theEdge1->Sense())
-	{
-		std::reverse(sub1.begin(), sub1.end());
-	}
-
-	RemoveEdge(theEdge0);
-	RemoveEdge(theEdge1);
-
-	for (const auto& x : sub0)
-	{
-		Debug_Null_Pointer(x);
-
-		auto edge = std::make_shared<Pln_Edge>();
-		Debug_Null_Pointer(edge);
-
-		Import(edge);
-	}
-
-	for (const auto& x : sub1)
-	{
-		Debug_Null_Pointer(x);
-
-		auto edge = std::make_shared<Pln_Edge>();
-		Debug_Null_Pointer(edge);
-
-		Import(edge);
-	}
-}
+//void tnbLib::Cad2d_Modeler::Trim
+//(
+//	const std::shared_ptr<Pln_Edge>& theEdge0, 
+//	const std::shared_ptr<Pln_Edge>& theEdge1
+//)
+//{
+//	if (NOT theEdge0)
+//	{
+//		FatalErrorIn("void Trim(const std::shared_ptr<Pln_Edge>& theEdge0, const std::shared_ptr<Pln_Edge>& theEdge1)")
+//			<< "the edge0 is null" << endl
+//			<< abort(FatalError);
+//	}
+//
+//	if (NOT theEdge1)
+//	{
+//		FatalErrorIn("void Trim(const std::shared_ptr<Pln_Edge>& theEdge0, const std::shared_ptr<Pln_Edge>& theEdge1)")
+//			<< "the edge1 is null" << endl
+//			<< abort(FatalError);
+//	}
+//
+//	auto intAlg = std::make_shared<Cad2d_EdgeEdgeIntersection>();
+//	Debug_Null_Pointer(intAlg);
+//
+//	intAlg->LoadEdge0(theEdge0);
+//	intAlg->LoadEdge1(theEdge1);
+//
+//	intAlg->Perform();
+//	Debug_If_Condition_Message(NOT intAlg->IsDone(), "the intersection algorithm is not performed!");
+//
+//	if (NOT intAlg->NbEntities())
+//	{
+//		return;
+//	}
+//
+//	const auto& pairList = intAlg->Entities();
+//
+//	entityList entities0, entities1;
+//	for (const auto& x : pairList)
+//	{
+//		Debug_Null_Pointer(x);
+//
+//		auto ent0 = std::dynamic_pointer_cast<Cad2d_IntsctEntity_Segment>(x->Entity0());
+//		Debug_Null_Pointer(ent0);
+//
+//		auto ent1 = std::dynamic_pointer_cast<Cad2d_IntsctEntity_Segment>(x->Entity1());
+//		Debug_Null_Pointer(ent1);
+//
+//		entities0.push_back(std::move(ent0));
+//		entities1.push_back(std::move(ent1));
+//	}
+//
+//	Cad2d_IntsctEntity_Segment::SortEntities(entities0);
+//	Cad2d_IntsctEntity_Segment::SortEntities(entities1);
+//
+//	auto sub0 = Cad2d_IntsctEntity_Segment::SubdivideEdge
+//	(
+//		theEdge0, entities0, 
+//		std::max(theEdge0->Vtx0()->Precision(), theEdge0->Vtx1()->Precision())
+//	);
+//
+//	if (NOT theEdge0->Sense())
+//	{
+//		std::reverse(sub0.begin(), sub0.end());
+//	}
+//
+//	auto sub1 = Cad2d_IntsctEntity_Segment::SubdivideEdge
+//	(
+//		theEdge1, entities1,
+//		std::max(theEdge1->Vtx0()->Precision(), theEdge1->Vtx1()->Precision())
+//	);
+//
+//	if (NOT theEdge1->Sense())
+//	{
+//		std::reverse(sub1.begin(), sub1.end());
+//	}
+//
+//	RemoveEdge(theEdge0);
+//	RemoveEdge(theEdge1);
+//
+//	for (const auto& x : sub0)
+//	{
+//		Debug_Null_Pointer(x);
+//
+//		auto edge = std::make_shared<Pln_Edge>();
+//		Debug_Null_Pointer(edge);
+//
+//		Import(edge);
+//	}
+//
+//	for (const auto& x : sub1)
+//	{
+//		Debug_Null_Pointer(x);
+//
+//		auto edge = std::make_shared<Pln_Edge>();
+//		Debug_Null_Pointer(edge);
+//
+//		Import(edge);
+//	}
+//}
 
 std::pair<Standard_Boolean, Standard_Integer>
 tnbLib::Cad2d_Modeler::Union
