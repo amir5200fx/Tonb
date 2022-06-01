6aeb3ace17457e15c29aada56e04d0c8ebfe476e
aasoleimani86 <aasoleimani86@gmail.com>
2022-05-31 20:04:24 -0700

    debugging the 5pts sample points

--- TnbGeo/TnbLib/Geo/SamplePoints/2d/5pts/Geo2d_SamplePoints_5Pts.cxx
+++ TnbGeo/TnbLib/Geo/SamplePoints/2d/5pts/Geo2d_SamplePoints_5Pts.cxx
@@ -7,11 +7,14 @@
 
 const Standard_Integer tnbLib::Geo2d_SamplePoints_5Pts::nbPts = 5;
 
-const tnbLib::Pnt2d tnbLib::Geo2d_SamplePoints_5Pts::P0(0, 0);
-const tnbLib::Pnt2d tnbLib::Geo2d_SamplePoints_5Pts::P1(1, 0);
-const tnbLib::Pnt2d tnbLib::Geo2d_SamplePoints_5Pts::P2(1, 1);
-const tnbLib::Pnt2d tnbLib::Geo2d_SamplePoints_5Pts::P3(0, 1);
-const tnbLib::Pnt2d tnbLib::Geo2d_SamplePoints_5Pts::P4(0.5, 0.5);
+
+tnbLib::Geo2d_SamplePoints_5Pts::Geo2d_SamplePoints_5Pts()
+	: P0(Pnt2d(0, 0))
+	, P1(Pnt2d(1, 0))
+	, P2(Pnt2d(1, 1))
+	, P3(Pnt2d(0, 1))
+	, P4(Pnt2d(0.5, 0.5))
+{}
 
 tnbLib::Pnt2d
 tnbLib::Geo2d_SamplePoints_5Pts::CalcPoint
