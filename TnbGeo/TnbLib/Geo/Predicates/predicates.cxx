1f8f3ee85a54a632a48a76698eedee831aafdc25
aasoleimani <aasoleimani86@gmail.com>
2022-08-07 22:47:36 -0700

    adding the predicates to the lib

---
+++ TnbGeo/TnbLib/Geo/Predicates/predicates.cxx
@@ -0,0 +1,18 @@
+#include <predicates.h>
+
+class PredicatesRunTime
+{
+
+	/*Private Data*/
+
+public:
+
+	// default constructor [8/3/2022 Amir]
+
+	PredicatesRunTime()
+	{
+		exactinit(1);
+	}
+};
+
+static const PredicatesRunTime PredicatesRunTimeObj;
