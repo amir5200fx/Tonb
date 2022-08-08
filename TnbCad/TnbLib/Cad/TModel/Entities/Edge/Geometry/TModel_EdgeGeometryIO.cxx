84fdb08dda88c88b792c88121b15877702528b8a
aasoleimani <aasoleimani86@gmail.com>
2022-08-07 22:40:06 -0700

    updating the edgeGeoemtry structure: adding mesh into it

--- TnbCad/TnbLib/Cad/TModel/Entities/Edge/Geometry/TModel_EdgeGeometryIO.cxx
+++ TnbCad/TnbLib/Cad/TModel/Entities/Edge/Geometry/TModel_EdgeGeometryIO.cxx
@@ -1,13 +1,16 @@
 #include <TModel_EdgeGeometry.hxx>
 
 #include <TModel_ParaCurve.hxx>
+#include <Entity3d_Polygon.hxx>
 
 TNB_SAVE_IMPLEMENTATION(tnbLib::TModel_EdgeGeometry)
 {
 	ar & theParaCurve_;
+	ar & theMesh_;
 }
 
 TNB_LOAD_IMPLEMENTATION(tnbLib::TModel_EdgeGeometry)
 {
 	ar & theParaCurve_;
+	ar & theMesh_;
 }
