643051cb3feeb4e0bec9fbef6762ad4c10fd0846
aasoleimani <aasoleimani86@gmail.com>
2022-08-07 22:55:55 -0700

    updating the tnbGlobal lib

--- TnbGlobal/include/Global_Serialization.hxx
+++ TnbGlobal/include/Global_Serialization.hxx
@@ -24,6 +24,7 @@
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/map.hpp>
+#include <boost/serialization/set.hpp>
 #include <boost/serialization/array.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/weak_ptr.hpp>
