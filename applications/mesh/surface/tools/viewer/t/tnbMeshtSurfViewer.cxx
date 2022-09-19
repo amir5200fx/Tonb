2e0b3d3d7bd0ec96ce23ddf6b5435f9e0b5c7df7
aasoleimani <aasoleimani86@gmail.com>
2022-09-20 00:00:06 +0430

    adding surface mesh viewer into the solution

---
+++ applications/mesh/surface/tools/viewer/t/tnbMeshtSurfViewer.cxx
@@ -0,0 +1,221 @@
+#include <Aft2d_tSolutionDataSurface.hxx>
+#include <Entity3d_Triangulation.hxx>
+#include <Entity3d_Box.hxx>
+#include <Global_File.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+	static unsigned short verbose = 0;
+	static bool loadTag = false;
+	static bool exeTag = false;
+
+	static std::shared_ptr<Aft2d_tSolutionDataSurface> mySoluData;
+	static std::string myFileName;
+
+	static const std::string loadExt = Aft2d_tSolutionDataSurface::extension;
+	static const std::string saveExt = Entity3d_Triangulation::extension + "list";
+
+	static std::vector<std::shared_ptr<Entity3d_Triangulation>> myTris;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void loadFile(const std::string& name)
+	{
+		file::CheckExtension(name);
+
+		mySoluData = file::LoadFile<std::shared_ptr<Aft2d_tSolutionDataSurface>>(name + loadExt, verbose);
+		myFileName = name;
+		if (NOT mySoluData)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the data solution file is null!" << endl
+				<< abort(FatalError);
+		}
+
+		loadTag = true;
+	}
+
+	void loadFile()
+	{
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), loadExt).string();
+		loadFile(name);
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+
+		file::CheckExtension(name);
+
+		file::SaveTo(myTris, name + saveExt, verbose);
+	}
+
+	void saveTo()
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+		saveTo(myFileName);
+	}
+
+	void execute()
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no file has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		const auto& tris = mySoluData->Tris();
+		myTris.reserve(tris.size());
+		for (const auto& x : tris)
+		{
+			myTris.push_back(x.second);
+		}
+
+		exeTag = true;
+
+		if (verbose)
+		{
+			Info << endl
+				<< " The application is performed, successfully!" << endl;
+		}
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFuns(const module_t& mod)
+	{
+		// io functions 
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadFile(name); }), "loadFile");
+
+		// settings 
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators 
+		mod->add(chaiscript::fun([]()-> void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char *argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << " This application is aimed to retrieve the triangulations from a solution data." << endl << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int); Levels: 0, 1, 2" << endl << endl
+
+				<< " # Operators:" << endl << endl
+
+				<< " - execute()" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFuns(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbMeshtSurfViewer"));
+
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
