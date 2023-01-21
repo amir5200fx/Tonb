16bb00d5b90a8c76c635399072b0aef6db9372cb
Mohammad Amin Yaaghoubi <amin-1387@hotmail.com>
2023-01-22 00:45:45 +0330

    add serialization for theFace_

--- TnbCad/TnbLib/Cad/GModel/Entities/Surface/Geometry/GModel_SurfaceGeometryIO.cxx
+++ TnbCad/TnbLib/Cad/GModel/Entities/Surface/Geometry/GModel_SurfaceGeometryIO.cxx
@@ -2,12 +2,35 @@
 
 #include <Cad_GeomSurface.hxx>
 
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <BRepTools.hxx>
+#include <BRep_Builder.hxx>
+
 TNB_SAVE_IMPLEMENTATION(tnbLib::GModel_SurfaceGeometry)
 {
 	ar & theGeometry_;
+
+	std::stringstream st;
+	st.precision(16);
+	BRepTools::Write(theFace_, st);
+
+	std::string s = st.str();
+	ar << s;
 }
 
 TNB_LOAD_IMPLEMENTATION(tnbLib::GModel_SurfaceGeometry)
 {
 	ar & theGeometry_;
+
+	std::string st;
+	ar >> st;
+
+	std::stringstream is;
+	is << st;
+
+	BRep_Builder b;
+	BRepTools::Read(theFace_, is, b);
 }
