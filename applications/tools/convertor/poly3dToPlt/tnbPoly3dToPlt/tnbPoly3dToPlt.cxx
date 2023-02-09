8c8c43c1a113e93864bbe5bb4820e2e71367fc4c
amir5200fx <aasoleimani86@gmail.com>
2023-02-09 17:14:31 +0330

    two application for drawing a polygon by tecplot

---
+++ applications/tools/convertor/poly3dToPlt/tnbPoly3dToPlt/tnbPoly3dToPlt.cxx
@@ -0,0 +1,189 @@
+#include <Entity3d_Polygon.hxx>
+#include <Global_File.hxx>
+#include <TnbError.hxx>
+#include <OFstream.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+
+	static const auto extension = Entity3d_Polygon::extension;
+
+	static unsigned short verbose(0);
+	static std::shared_ptr<Entity3d_Polygon> myPoly;
+
+	static std::string myFileName;
+
+	static bool loadTag = false;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void loadFile(const std::string& name)
+	{
+		file::CheckExtension(name);
+
+		myPoly = file::LoadFile<std::shared_ptr<Entity3d_Polygon>>(name + extension, verbose);
+		if (NOT myPoly)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the edge is null" << endl
+				<< abort(FatalError);
+		}
+		loadTag = true;
+	}
+
+	void loadFile()
+	{
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), extension).string();
+		myFileName = name;
+		loadFile(name);
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no mesh has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		fileName fn(name + ".plt");
+		OFstream myFile(fn);
+
+		myPoly->ExportToPlt(myFile);
+	}
+
+	void saveTo()
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no mesh has been loaded!" << endl
+				<< abort(FatalError);
+		}
+		saveTo(myFileName);
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFunctions(const module_t& mod)
+	{
+		//- io functions
+
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadFile(name); }), "loadFile");
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+
+		//- settings
+
+		mod->add(chaiscript::fun([](unsigned short i)->void {verbose = i; }), "setVerbose");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	//sysLib::init_gl_marine_integration_info();
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << endl;
+			Info << " This application is aimed to convert a polygon file to the *.plt file format." << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int);    - Levels: 0, 1" << endl << endl
+
+				<< " # operators: " << endl
+
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFunctions(mod);
+
+			chai.add(mod);
+
+			//std::string address = ".\\system\\mesh2dToPlt";
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbPoly3dToPlt"));
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
