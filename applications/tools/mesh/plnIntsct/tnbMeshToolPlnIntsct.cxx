86362bced912e7fac91745576a0b34623973702b
amir5200fx <aasoleimani86@gmail.com>
2023-02-09 17:15:32 +0330

    an application to calculate the intersections between a plane and a mesh

---
+++ applications/tools/mesh/plnIntsct/tnbMeshToolPlnIntsct.cxx
@@ -0,0 +1,253 @@
+#include <GeoIntsct3d_PlnTris.hxx>
+#include <Entity3d_Triangulation.hxx>
+#include <Geo_Serialization.hxx>
+#include <Global_File.hxx>
+#include <Global_Timer.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+#include <gp_Pln.hxx>
+
+namespace tnbLib
+{
+
+	static unsigned short verbose = 0;
+	static bool loadTag = false;
+	static bool exeTag = false;
+
+	static std::shared_ptr<gp_Pln> myPlane;
+	static std::shared_ptr<Entity3d_Triangulation> myTris;
+	static const std::string extension = GeoIntsct3d_PlnTris::intsctEntity::extension;
+
+	static std::vector<std::shared_ptr<GeoIntsct3d_PlnTris::intsctEntity>> myEntities;
+
+	static std::string myFileName;
+
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
+	void checkFolder(const std::string& name)
+	{
+		if (NOT boost::filesystem::is_directory(name))
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no {" << name << "} directory has been found!" << endl
+				<< abort(FatalError);
+		}
+	}
+
+	void saveTo(const std::string& name)
+	{
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the application is not performed!" << endl
+				<< abort(FatalError);
+		}
+
+		file::CheckExtension(name);
+
+		file::SaveTo(myEntities, name + extension, verbose);
+	}
+
+	auto loadPlane()
+	{
+		checkFolder("plane");
+
+		const auto currentPath = boost::filesystem::current_path();
+
+		// change the current path [2/6/2023 Payvand]
+		boost::filesystem::current_path(currentPath.string() + R"(\plane)");
+
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), ".gpln").string();
+
+		auto plane = file::LoadFile<std::shared_ptr<gp_Pln>>(name + ".gpln", verbose);
+
+		//- change back the current path
+		boost::filesystem::current_path(currentPath);
+
+		return std::move(plane);
+	}
+
+	auto loadMesh()
+	{
+		checkFolder("mesh");
+
+		const auto currentPath = boost::filesystem::current_path();
+
+		// change the current path [2/6/2023 Payvand]
+		boost::filesystem::current_path(currentPath.string() + R"(\mesh)");
+
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), Entity3d_Triangulation::extension).string();
+
+		auto mesh = file::LoadFile<std::shared_ptr<Entity3d_Triangulation>>(name + Entity3d_Triangulation::extension, verbose);
+		if (NOT mesh)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " the mesh file is null!" << endl
+				<< abort(FatalError);
+		}
+
+		//- change back the current path
+		boost::filesystem::current_path(currentPath);
+
+		return std::move(mesh);
+	}
+
+	void loadFiles()
+	{
+		myPlane = loadPlane();
+		myTris = loadMesh();
+
+		loadTag = true;
+	}
+
+	void execute()
+	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< " no file has been loaded!" << endl
+				<< abort(FatalError);
+		}
+
+		GeoIntsct3d_PlnTris alg;
+		alg.SetPlane(myPlane);
+		alg.SetMesh(myTris);
+
+		alg.Perform();
+
+		myEntities = alg.Entities();
+
+		exeTag = true;
+
+		if (verbose)
+		{
+			Info << endl
+				<< " the application is performed, successfully!" << endl;
+		}
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFunctions(const module_t& mod)
+	{
+		// io functions [12/5/2021 Amir]
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {loadFiles(); }), "loadFiles");
+
+		// settings [12/5/2021 Amir]
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators [12/5/2021 Amir]
+		mod->add(chaiscript::fun([]()->void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << " This application is aimed to calculate the intersection between plane and mesh." << endl << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFiles()" << endl
+				<< " - saveTo(name)" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int); Levels: 0, 1" << endl << endl
+
+				<< " # Operators:" << endl << endl
+
+				<< " - execute()" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFunctions(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbMeshToolPlnIntsct"));
+
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
+}
