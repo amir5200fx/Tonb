bbd450ee0ebc53ea3b53702359666daeac6d7859
amir5200fx <aasoleimani86@gmail.com>
2023-05-24 12:47:47 +0100

    adding the offsets of two airfoil types  to the geo lib: MH102, S833

---
+++ TnbGeo/TnbLib/Geo/AirfoilOffsets/MH102/Geo_AirfoilOffsets_MH102.cxx
@@ -0,0 +1,187 @@
+#include <Geo_AirfoilOffsets_MH102.hxx>
+
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P0(1.0, 0.0);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P1(0.99615, 0.000637);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P2(0.985206, 0.002822);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P3(0.968306, 0.006632);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P4(0.946026, 0.011471);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P5(0.918399, 0.017076);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P6(0.885721, 0.023612);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P7(0.84862, 0.031107);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P8(0.807743, 0.039438);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P9(0.763753, 0.048413);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P10(0.717309, 0.057791);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P11(0.669052, 0.067289);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P12(0.619598, 0.076611);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P13(0.569531, 0.08545);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P14(0.519404, 0.093513);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P15(0.469731, 0.100522);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P16(0.420999, 0.106226);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P17(0.373661, 0.11041);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P18(0.3282, 0.112841);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P19(0.284912, 0.113169);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P20(0.243808, 0.111302);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P21(0.20506, 0.107464);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P22(0.169013, 0.101817);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P23(0.135941, 0.094472);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P24(0.106083, 0.085593);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P25(0.079642, 0.075365);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P26(0.056785, 0.064018);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P27(0.037648, 0.05182);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P28(0.022333, 0.03909);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P29(0.010902, 0.026213);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P30(0.003431, 0.013721);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P31(0.000818, 0.006249);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::upper::P32(0.0002, 0.002938);
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::upper::nbPnts = 33;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			namespace upper
+			{
+				const std::vector<tnbLib::Pnt2d> coords =
+				{ P0,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,
+				P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,P24,P25,
+				P26,P27,P28,P29,P30,P31,P32 };
+			}
+		}
+	}
+}
+
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P0(8.2e-7, -0.00018);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P1(0.00012, -0.00213);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P2(0.000497, -0.00394);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P3(0.001208, -0.00566);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P4(0.002241, -0.00742);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P5(0.004264, -0.0101);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P6(0.006801, -0.01281);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P7(0.010501, -0.01613);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P8(0.023994, -0.02526);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P9(0.042013, -0.03405);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P10(0.064177, -0.04213);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P11(0.090201, -0.04904);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P12(0.119957, -0.05434);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P13(0.153499, -0.05771);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P14(0.190947, -0.0591);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P15(0.232248, -0.05877);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P16(0.277092, -0.057);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P17(0.325077, -0.05405);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P18(0.375706, -0.05021);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P19(0.428391, -0.04576);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P20(0.482434, -0.04098);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P21(0.537074, -0.03602);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P22(0.591624, -0.03098);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P23(0.64541, -0.02602);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P24(0.697728, -0.02126);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P25(0.747883, -0.01683);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P26(0.795201, -0.01282);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P27(0.839027, -0.0093);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P28(0.878737, -0.00633);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P29(0.913744, -0.00395);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P30(0.943495, -0.00217);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P31(0.967474, -0.00097);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P32(0.985201, -0.0003);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P33(0.996222, -5.6e-5);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::lower::P34(1.0, 0.0);
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::lower::nbPnts = 35;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			namespace lower
+			{
+				const std::vector<tnbLib::Pnt2d> coords =
+				{ P0,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,
+				P13,P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,
+				P24,P25,P26,P27,P28,P29,P30,P31,P32,P33,P34 };
+			}
+		}
+	}
+}
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::nbPnts = 68;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			const std::vector<tnbLib::Pnt2d> coords =
+			{ upper::P0,
+			upper::P1,
+			upper::P2,
+			upper::P3,
+			upper::P4,
+			upper::P5,
+			upper::P6,
+			upper::P7,
+			upper::P8,
+			upper::P9,
+			upper::P10,
+			upper::P11,
+			upper::P12,
+			upper::P13,
+			upper::P14,
+			upper::P15,
+			upper::P16,
+			upper::P17,
+			upper::P18,
+			upper::P19,
+			upper::P20,
+			upper::P21,
+			upper::P22,
+			upper::P23,
+			upper::P24,
+			upper::P25,
+			upper::P26,
+			upper::P27,
+			upper::P28,
+			upper::P29,
+			upper::P30,
+			upper::P31,
+			upper::P32,
+			lower::P0,
+			lower::P1,
+			lower::P2,
+			lower::P3,
+			lower::P4,
+			lower::P5,
+			lower::P6,
+			lower::P7,
+			lower::P8,
+			lower::P9,
+			lower::P10,
+			lower::P11,
+			lower::P12,
+			lower::P13,
+			lower::P14,
+			lower::P15,
+			lower::P16,
+			lower::P17,
+			lower::P18,
+			lower::P19,
+			lower::P20,
+			lower::P21,
+			lower::P22,
+			lower::P23,
+			lower::P24,
+			lower::P25,
+			lower::P26,
+			lower::P27,
+			lower::P28,
+			lower::P29,
+			lower::P30,
+			lower::P31,
+			lower::P32,
+			lower::P33,
+			lower::P34 };
+		}
+	}
+}
---
+++ TnbGeo/TnbLib/Geo/AirfoilOffsets/S833/Geo_AirfoilOffsets_S833.cxx
@@ -0,0 +1,192 @@
+#include <Geo_AirfoilOffsets_S833.hxx>
+
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P0(1.0, 0.0);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P1(0.996065, 0.001004);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P2(0.985020,   0.004646);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P3(0.968439,   0.011090);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P4(0.947640,   0.019540);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P5(0.923225,   0.028827);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P6(0.895095,   0.037957);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P7(0.862843,   0.046779);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P8(0.826835,   0.055487);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P9(0.787593,   0.063938);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P10(0.745618,   0.071919);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P11(0.701398,   0.079186);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P12(0.655387,   0.085456);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P13(0.607899,   0.090488);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P14(0.559249,   0.094305);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P15(0.510051,   0.096851);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P16(0.460675,   0.097982);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P17(0.411514,   0.097776);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P18(0.363095,   0.096316);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P19(0.315946,   0.093639);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P20(0.270564,   0.089788);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P21(0.227440,   0.084804);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P22(0.187003,   0.078746);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P23(0.149688,   0.071682);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P24(0.115830,   0.063700);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P25(0.085796,   0.054909);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P26(0.059807,   0.045441);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P27(0.038158,   0.035491);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P28(0.021003,   0.025264);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P29(0.008633,   0.015086);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P30(0.001918,   0.006451);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P31(0.001336,   0.005330);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P32(0.000805,   0.004131);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::upper::P33(0.000137,   0.001900);
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::S833::upper::nbPnts = 34;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			namespace S833
+			{
+				namespace upper
+				{
+					const std::vector<tnbLib::Pnt2d> coords =
+					{ P0,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,
+					P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,P24,P25,
+					P26,P27,P28,P29,P30,P31,P32, P33 };
+				}
+			}
+		}
+	}
+}
+
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P0(0.000005, -0.000388);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P1(0.000267, -0.002838);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P2(0.000510, -0.004055);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P3(0.005161, -0.014877);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P4(0.014227, -0.026119);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P5(0.027615, -0.037318);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P6(0.045093, -0.047937);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P7(0.066696, -0.057725);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P8(0.092161, -0.066350);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P9(0.121486, -0.073652);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P10(0.154343, -0.079439);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P11(0.190655, -0.083625);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P12(0.230021, -0.086135);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P13(0.272278, -0.086956);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P14(0.316953, -0.086108);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P15(0.363791, -0.083647);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P16(0.412249, -0.079666);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P17(0.461977, -0.074263);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P18(0.512361, -0.067552);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P19(0.563000, -0.059518);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P20(0.613510, -0.050246);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P21(0.663842, -0.039707);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P22(0.713990, -0.028763);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P23(0.763368, -0.018233);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P24(0.811239, -0.008893);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P25(0.856691, -0.001519);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P26(0.898436, 0.003017);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P27(0.934459, 0.004533);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P28(0.963162, 0.003879);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P29(0.983725, 0.002236);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P30(0.995960, 0.000675);
+const tnbLib::Pnt2d tnbLib::geoLib::airfoilOffsetsLib::S833::lower::P31(1.000000, 0.000000);
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::S833::lower::nbPnts = 32;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			namespace S833
+			{
+				namespace lower
+				{
+					const std::vector<tnbLib::Pnt2d> coords =
+					{ P0,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,
+					P13,P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,
+					P24,P25,P26,P27,P28,P29,P30,P31 };
+				}
+			}
+		}
+	}
+}
+
+const size_t tnbLib::geoLib::airfoilOffsetsLib::S833::nbPnts = 68;
+namespace tnbLib
+{
+	namespace geoLib
+	{
+		namespace airfoilOffsetsLib
+		{
+			namespace S833
+			{
+				const std::vector<tnbLib::Pnt2d> coords =
+				{ upper::P0,
+				upper::P1,
+				upper::P2,
+				upper::P3,
+				upper::P4,
+				upper::P5,
+				upper::P6,
+				upper::P7,
+				upper::P8,
+				upper::P9,
+				upper::P10,
+				upper::P11,
+				upper::P12,
+				upper::P13,
+				upper::P14,
+				upper::P15,
+				upper::P16,
+				upper::P17,
+				upper::P18,
+				upper::P19,
+				upper::P20,
+				upper::P21,
+				upper::P22,
+				upper::P23,
+				upper::P24,
+				upper::P25,
+				upper::P26,
+				upper::P27,
+				upper::P28,
+				upper::P29,
+				upper::P30,
+				upper::P31,
+				upper::P32,
+				upper::P33,
+				lower::P0,
+				lower::P1,
+				lower::P2,
+				lower::P3,
+				lower::P4,
+				lower::P5,
+				lower::P6,
+				lower::P7,
+				lower::P8,
+				lower::P9,
+				lower::P10,
+				lower::P11,
+				lower::P12,
+				lower::P13,
+				lower::P14,
+				lower::P15,
+				lower::P16,
+				lower::P17,
+				lower::P18,
+				lower::P19,
+				lower::P20,
+				lower::P21,
+				lower::P22,
+				lower::P23,
+				lower::P24,
+				lower::P25,
+				lower::P26,
+				lower::P27,
+				lower::P28,
+				lower::P29,
+				lower::P30,
+				lower::P31 };
+			}
+		}
+	}
+}
