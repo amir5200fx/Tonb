1f51c77c934f4b33f74ef8cb803f67a641fd9de7
amir5200fx <aasoleimani86@gmail.com>
2023-06-17 21:48:05 +0100

    updating the API

---
+++ API/win/createDirectories/tnbApiWinCreateDirectories/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tnbApiWinCreateDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: CreateFolders <n>");
+                return;
+            }
+
+            int n;
+            if (!int.TryParse(args[0], out n))
+            {
+                throw new ArgumentException("The argument must be an integer.");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                string folderName = i.ToString();
+                Directory.CreateDirectory(folderName);
+            }
+        }
+    }
+}
---
+++ API/win/createPath/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the current working directory
+            string directory = Directory.GetCurrentDirectory();
+
+            // Check if user has provided address argument
+            string address;
+            if (args.Length == 0)
+            {
+                address = directory;
+            }
+            else
+            {
+                // Get the address from the argument
+                address = args[0];
+            }
+
+            // Create a unique filename with .PATH extension in the current working directory
+            //string path = Path.Combine(directory, $"{Guid.NewGuid()}.PATH");
+            string path = Path.Combine(directory, $".PATH");
+
+            // Write the address to the file
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(address);
+            }
+
+            Console.WriteLine($"File created at: {path}");
+        }
+    }
+}
