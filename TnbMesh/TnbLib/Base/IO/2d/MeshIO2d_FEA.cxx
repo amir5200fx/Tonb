af6e4ee08137c30bef79db3a49bf17a0763e5efb
amir5200fx <aasoleimani86@gmail.com>
2023-07-04 23:33:46 +0100

    adding the io file format for finite element analysis

---
+++ TnbMesh/TnbLib/Base/IO/2d/MeshIO2d_FEA.cxx
@@ -0,0 +1,15 @@
+#include <MeshIO2d_FEA.hxx>
+
+#include <Entity2d_Triangulation.hxx>
+
+TNB_SAVE_IMPLEMENTATION(tnbLib::MeshIO2d_FEA)
+{
+	ar& theMesh_;
+	ar& theBoundaries_;
+}
+
+TNB_LOAD_IMPLEMENTATION(tnbLib::MeshIO2d_FEA)
+{
+	ar& theMesh_;
+	ar& theBoundaries_;
+}
