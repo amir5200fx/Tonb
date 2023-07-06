930d885ee225b7393103e23261dc7737a26aabbd
amir5200fx <aasoleimani86@gmail.com>
2023-07-06 19:46:37 +0100

    updating the applications

--- applications/mesh/plane/tools/exportToFEA/tnbMeshToolPlaneExportToFEA.cxx
+++ applications/mesh/plane/tools/exportToFEA/tnbMeshToolPlaneExportToFEA.cxx
@@ -30,6 +30,7 @@ namespace tnbLib
 
 	static unsigned short verbose = 0;
 	static bool loadTag = false;
+	static bool exeTag = false;
 
 	static std::shared_ptr<Aft2d_SolutionDataBase> mySoluData;
 	static std::string myFileName;
@@ -37,10 +38,17 @@ namespace tnbLib
 	static const std::string sub_directory = "soluData";
 	static std::map<word, std::shared_ptr<std::vector<connectivity::dual>>> myPhysics;
 
+	void setVerbose(unsigned int i)
+	{
+		Info << endl;
+		Info << " - the verbosity level is set to: " << i << endl;
+		verbose = i;
+	}
+
 	void loadFile(const std::string& name)
 	{
 		file::CheckExtension(name);
-
+		myFileName = name;
 		mySoluData = file::LoadFile<std::shared_ptr<Aft2d_SolutionDataBase>>(name + extension, verbose);
 		if (NOT mySoluData)
 		{
@@ -54,17 +62,60 @@ namespace tnbLib
 
 	void loadFile()
 	{
-		auto name = file::GetSingleFile(boost::filesystem::current_path(), extension).string();
-		myFileName = name;
+		auto name = file::GetSingleFile(boost::filesystem::current_path(), extension).string();		
 		loadFile(name);
 	}
 
 	void saveTo(const std::string& name)
 	{
-		const auto& mesh = mySoluData->Mesh();
-		auto fea = std::make_shared<MeshIO2d_FEA>();
-		fea->SetMesh(mesh);
-		fea->SetBoundaries(myPhysics);
+		if (NOT exeTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "the application is not performed." << endl
+				<< abort(FatalError);
+		}
+		if (mySoluData->IsIso())
+		{
+			auto soluData = std::dynamic_pointer_cast<Aft2d_SolutionData>(mySoluData);
+			Debug_Null_Pointer(soluData);
+			const auto& elements = soluData->Elements();
+			if (elements.empty())
+			{
+				FatalErrorIn(FunctionSIG)
+					<< "no element has been detected!" << endl
+					<< abort(FatalError);
+			}
+			auto mesh = Aft_Tools::RetrieveTriangleMesh(elements);
+			auto fea = std::make_shared<MeshIO2d_FEA>();
+			fea->SetMesh(std::move(mesh));
+			fea->SetBoundaries(myPhysics);
+
+			file::SaveTo(fea, name + MeshIO2d_FEA::extension, verbose);
+		}
+		else
+		{
+			auto soluData = std::dynamic_pointer_cast<Aft2d_SolutionDataAnIso>(mySoluData);
+			Debug_Null_Pointer(soluData);
+			const auto& elements = soluData->Elements();
+			if (elements.empty())
+			{
+				FatalErrorIn(FunctionSIG)
+					<< "no element has been detected!" << endl
+					<< abort(FatalError);
+			}
+			auto mesh = Aft_Tools::RetrieveTriangleMesh(elements);
+			auto fea = std::make_shared<MeshIO2d_FEA>();
+			fea->SetMesh(std::move(mesh));
+			fea->SetBoundaries(myPhysics);
+
+			file::SaveTo(fea, name + MeshIO2d_FEA::extension, verbose);
+		}
+		
+	}
+
+	void saveTo()
+	{
+		saveTo(myFileName);
 	}
 
 	void findPhysics
@@ -231,6 +282,12 @@ namespace tnbLib
 
 	void execute()
 	{
+		if (NOT loadTag)
+		{
+			FatalErrorIn(FunctionSIG)
+				<< "no file has been loaded." << endl
+				<< abort(FatalError);
+		}
 		std::map<word, std::vector<std::shared_ptr<Entity2d_Polygon>>> physicMap;
 		if (mySoluData->IsIso())
 		{
@@ -260,6 +317,15 @@ namespace tnbLib
 						physicMap.insert(std::move(paired));
 					}
 					physicMap.at(physic).push_back(bmesh);
+				}
+			}
+
+			if (verbose)
+			{
+				Info << " # no. of physics: " << physicMap.size() << endl;
+				for (const auto& x : physicMap)
+				{
+					Info << " - " << x.first << endl;
 				}
 			}
 			
@@ -306,6 +372,15 @@ namespace tnbLib
 				}
 			}
 
+			if (verbose)
+			{
+				Info << " # no. of physics: " << physicMap.size() << endl;
+				for (const auto& x : physicMap)
+				{
+					Info << " - " << x.first << endl;
+				}
+			}
+
 			auto nodes = Aft_Tools::RetrieveNodes(soluData->Elements());
 			std::vector<std::shared_ptr<Aft2d_NodeAnIso>> bnodes;
 			for (const auto& x : nodes)
@@ -318,5 +393,137 @@ namespace tnbLib
 
 			findPhysics(physicMap, bnodes);
 		}
+
+		exeTag = true;
+		if (verbose)
+		{
+			Info << endl
+				<< " - the application is successfully performed." << endl;
+		}
 	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setFuns(const module_t& mod)
+	{
+		// io functions 
+		mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {saveTo(); }), "saveTo");
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadFile(name); }), "loadFile");
+
+		// settings 
+
+		mod->add(chaiscript::fun([](unsigned short i)-> void {setVerbose(i); }), "setVerbose");
+
+		// operators 
+		mod->add(chaiscript::fun([]()-> void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << " This application is aimed to export a finite element mesh from a solution data of a mesh." << endl << endl;
+
+			Info << " subdirectory: {" << sub_directory << "}" << endl;
+			Info << " input: *" << Aft2d_SolutionDataBase::extension << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # IO functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int); Levels: 0, 1" << endl << endl
+
+				<< " # Operators:" << endl << endl
+				
+				<< " - execute()" << endl
+				<< endl;
+			return 0;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setFuns(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbMeshToolPlaneExportToFEA"));
+
+				chai.eval_file(myFileName);
+				return 0;
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+		else
+		{
+			Info << " - No valid command is entered" << endl
+				<< " - For more information use '--help' command" << endl;
+			FatalError.exit();
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+	return 1;
 }
---
+++ applications/tools/convertor/mesh2dToOpenFOAM/tnbMesh2dToOpenFOAM/tnbMesh2dToOpenFOAM.cxx
@@ -0,0 +1,189 @@
+#include <MeshPost2d_OFTopology.hxx>
+#include <MeshIO2d_FEA.hxx>
+#include <Global_File.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+namespace tnbLib
+{
+
+	static std::shared_ptr<MeshIO2d_FEA> myMeshIO;
+
+	TNB_DEFINE_VERBOSE_OBJ;
+	TNB_DEFINE_EXETAG_OBJ;
+	TNB_DEFINE_LOADTAG_OBJ;
+	TNB_DEFINE_GLOBAL_PATH;
+	TNB_DEFINE_FILENAME_OBJ;
+	static const std::string sub_directory = "mesh";
+
+	TNB_SET_VERBOSE_FUN;
+	
+
+	void loadMesh(const std::string& name)
+	{
+		file::CheckExtension(name);
+		myMeshIO = 
+			file::LoadFile<std::shared_ptr<MeshIO2d_FEA>>(name + MeshIO2d_FEA::extension, verbose);
+		TNB_CHECK_LOADED_FILE(myMeshIO);
+		loadTag = true;
+		myFileName = name;
+	}
+
+	void loadMesh()
+	{
+		myMeshIO = 
+			file::LoadSingleFile<std::shared_ptr<MeshIO2d_FEA>>
+			(sub_directory, MeshIO2d_FEA::extension, verbose, myFileName);
+		loadTag = true;
+		TNB_CHECK_LOADED_FILE(myMeshIO);
+	}
+
+	void loadFile()
+	{
+		if (file::IsDirectory(sub_directory))
+		{
+			loadMesh();
+		}
+		else
+		{
+			auto name = 
+				file::GetSingleFile(file::GetCurrentPath(), MeshIO2d_FEA::extension).string();
+			loadMesh(name);
+		}
+	}
+
+	void execute()
+	{
+		TNB_CHECK_LOAD_TAG;
+		auto topology = std::make_shared<MeshPost2d_OFTopology>();
+		topology->SetMeshIO(myMeshIO);
+		topology->Perform();
+		for (const auto& x : topology->Interiors())
+		{
+			std::cout << "id: " << x.Index() << ", owner: " << x.Owner() << ", neighbor: " << x.Neighbor() << std::endl;
+		}
+		PAUSE;
+		for (const auto& x : topology->Boundaries())
+		{
+			for (const auto& p : x.second)
+			{
+				std::cout << "physic: " << x.first << ", id: " << p.Index() << ", owner: " << p.Owner() << ", neighbor: " << p.Neighbor() << std::endl;
+			}
+			
+		}
+		TNB_PERFORMED_TAG;
+	}
+}
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <chaiscript/chaiscript.hpp>
+
+namespace tnbLib
+{
+
+	typedef std::shared_ptr<chaiscript::Module> module_t;
+
+	void setGlobals(const module_t& mod)
+	{
+		// io functions [1/23/2023 Payvand]
+		mod->add(chaiscript::fun([]()-> void {loadFile(); }), "loadFile");
+		mod->add(chaiscript::fun([](const std::string& name)-> void {loadMesh(name); }), "loadFile");
+		//mod->add(chaiscript::fun([](const std::string& name)-> void {saveTo(name); }), "saveTo");
+
+		// settings [1/23/2023 Payvand]
+		mod->add(chaiscript::fun([](unsigned short c)->void {setVerbose(c); }), "setVerbose");
+
+		// operators [1/23/2023 Payvand]
+		mod->add(chaiscript::fun([]()-> void {execute(); }), "execute");
+	}
+
+	std::string getString(char* argv)
+	{
+		std::string argument(argv);
+		return std::move(argument);
+	}
+
+	Standard_Boolean IsEqualCommand(char* argv, const std::string& command)
+	{
+		auto argument = getString(argv);
+		return argument IS_EQUAL command;
+	}
+}
+
+using namespace tnbLib;
+
+int main(int argc, char* argv[])
+{
+	//FatalError.throwExceptions();
+
+	if (argc <= 1)
+	{
+		Info << " - No command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+
+	if (argc IS_EQUAL 2)
+	{
+		if (IsEqualCommand(argv[1], "--help"))
+		{
+			Info << endl;
+			Info << " This application is aimed to convert a two-dimensional mesh to OpenFOAM mesh." << endl << endl;
+
+			Info << " - subdirectory: {" << sub_directory << "}" << endl;
+			Info << endl
+				<< " Function list:" << endl << endl
+
+				<< " # I/O functions: " << endl << endl
+
+				<< " - loadFile(name [optional])" << endl
+				<< " - saveTo(name [optional])" << endl << endl
+
+				<< " # Operators: " << endl << endl
+				<< " - execute()" << endl << endl
+
+				<< " # Settings: " << endl << endl
+
+				<< " - setVerbose(unsigned int);    - Levels: 0, 1" << endl
+				<< endl;
+		}
+		else if (IsEqualCommand(argv[1], "--run"))
+		{
+			chaiscript::ChaiScript chai;
+
+			auto mod = std::make_shared<chaiscript::Module>();
+
+			setGlobals(mod);
+
+			chai.add(mod);
+
+			try
+			{
+				fileName myFileName(file::GetSystemFile("tnbMesh2dToOpenFOAM"));
+
+				chai.eval_file(myFileName);
+			}
+			catch (const chaiscript::exception::eval_error& x)
+			{
+				Info << x.pretty_print() << endl;
+			}
+			catch (const error& x)
+			{
+				Info << x.message() << endl;
+			}
+			catch (const std::exception& x)
+			{
+				Info << x.what() << endl;
+			}
+		}
+	}
+	else
+	{
+		Info << " - No valid command is entered" << endl
+			<< " - For more information use '--help' command" << endl;
+		FatalError.exit();
+	}
+}
