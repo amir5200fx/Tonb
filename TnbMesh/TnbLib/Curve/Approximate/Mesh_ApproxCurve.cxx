1a949c0ce0df7e24187b554be35a9a3271062d5d
amir5200fx <aasoleimani86@gmail.com>
2024-07-14 16:01:54 +0100

    Changing the output result from a polygon to a parameter list

--- TnbMesh/TnbLib/Curve/Approximate/Mesh_ApproxCurve.cxx
+++ TnbMesh/TnbLib/Curve/Approximate/Mesh_ApproxCurve.cxx
@@ -1,1 +1,18 @@
 #include <Mesh_ApproxCurve.hxx>
+
+std::vector<Standard_Real> tnbLib::Mesh_ApproxCurveBase::Tessellate(const std::vector<Standard_Real>& us,
+	Standard_Integer theMinSamples)
+{
+	std::vector<Standard_Real> ts;
+	for (size_t i = 0; i < us.size() - 1; i++)
+	{
+		ts.emplace_back(us.at(i));
+		ts.emplace_back(MEAN(us.at(i), us.at(i + 1)));
+	}
+	ts.emplace_back(us.at(us.size() - 1));
+	if (ts.size() < theMinSamples)
+	{
+		return Tessellate(ts, theMinSamples);
+	}
+	return std::move(ts);
+}
