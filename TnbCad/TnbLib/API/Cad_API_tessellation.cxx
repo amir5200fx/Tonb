1df1ec8927b4dc6e265bc8a6b77c77faf0ee0c81
amir5200fx <aasoleimani86@gmail.com>
2024-08-25 17:51:47 +0100

    The tessellating shape feature has been added to the API.

---
+++ TnbCad/TnbLib/API/Cad_API_tessellation.cxx
@@ -0,0 +1,63 @@
+#include <Cad_API.hxx>
+
+#include <Cad_Tools.hxx>
+#include <Cad_Shape.hxx>
+#include <Cad_FastDiscrete.hxx>
+#include <FastDiscrete_Params.hxx>
+#include <Geo_Tools.hxx>
+#include <Global_Timer.hxx>
+#include <TnbError.hxx>
+#include <OSstream.hxx>
+
+#ifdef DebugInfo
+#undef DebugInfo
+#endif // DebugInfo
+
+#include <Poly_Triangulation.hxx>
+
+tnbLib::api::cad::Tessellation::Tessellation(const chai::Cad&)
+	: obj{ std::make_shared<FastDiscrete_Params>() }
+{
+	obj->Angle = Geo_Tools::DegToRadian(5.0);
+	obj->Deflection = 1.e-3;
+	obj->MinSize = 1.e-5;
+	obj->InParallel = false;
+	obj->Relative = true;
+	obj->AdaptiveMin = false;
+	obj->InternalVerticesMode = true;
+	obj->ControlSurfaceDeflection = true;
+}
+
+
+void tnbLib::api::cad::tessellate(const Shape& shape, const Tessellation& tessellation, unsigned short verbose)
+{
+	{
+		Global_Timer timer;
+		timer.SetInfo(Global_TimerInfo_ms);
+		Cad_FastDiscrete::Triangulation(shape.shape->Shape(), *tessellation.obj);
+	}
+	if (verbose)
+	{
+		const auto surfaces = Cad_Tools::RetrieveTriangulation(shape.shape->Shape());
+		Info << " - nb. of surfaces: " << surfaces.size() << "\n";
+		size_t k = 0;
+		size_t nbNodes = 0;
+		size_t nbTris = 0;
+		for (const auto& x : surfaces)
+		{
+			if (x)
+			{
+				Info << " - surface nb. " << k++ << ": "
+					<< "nb. of nodes: " << x->NbNodes() << ", nb. of triangles: " << x->NbTriangles()
+					<< ", Deflection: " << x->Deflection() << "\n";
+
+				nbNodes += x->NbNodes();
+				nbTris += x->NbTriangles();
+			}
+		}
+		Info << "\n";
+		Info << " - Total nb. of nodes: " << nbNodes << "\n";
+		Info << " - Total nb. of triangles: " << nbTris << "\n";
+		Info << "\n";
+	}
+}
