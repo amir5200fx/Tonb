4499e60ef15a34cb7890038e5bd9e6498724879d
Amir Amir-Solaymani <Amir@ilosta.com>
2025-04-19 11:16:11 +0100

    updating the global_bound

--- TnbGlobal/include/Global_Bound.hxx
+++ TnbGlobal/include/Global_Bound.hxx
@@ -81,22 +81,104 @@ namespace tnbLib
 		}
 	};
 
+	template<>
+	class Global_Bound<Standard_Integer>
+	{
+		/*Private Data*/
+		Standard_Integer theLower_;
+		Standard_Integer theUpper_;
+
+		//- Private functions and operators
+		friend class boost::serialization::access;
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& theLower_;
+			ar& theUpper_;
+		}
+	protected:
+		// default constructor
+		Global_Bound() = default;
+
+		// constructors
+		Global_Bound(Standard_Integer lower, Standard_Integer upper)
+			: theLower_(lower)
+			, theUpper_(upper)
+		{}
+	public:
+		// Public functions and operators
+
+		auto Lower() const { return theLower_; }
+		auto Upper() const { return theUpper_; }
+		auto& Lower() { return theLower_; }
+		auto& Upper() { return theUpper_; }
+
+		auto IsInside(Standard_Integer x) const
+		{
+			return INSIDE(x, Lower(), Upper());
+		}
+
+		void SetLower(Standard_Integer lower)
+		{
+			theLower_ = lower;
+		}
+		void SetUpper(Standard_Integer upper)
+		{
+			theUpper_ = upper;
+		}
+	};
+
+	template<>
+	class Global_Bound<Standard_Real>
+	{
+		/*Private Data*/
+		Standard_Real theLower_;
+		Standard_Real theUpper_;
+
+		//- Private functions and operators
+		friend class boost::serialization::access;
+		template<class Archive>
+		void serialize(Archive& ar, const unsigned int version)
+		{
+			ar& theLower_;
+			ar& theUpper_;
+		}
+	protected:
+		// default constructor
+		Global_Bound() = default;
+
+		// constructors
+		Global_Bound(Standard_Real lower, Standard_Real upper)
+			: theLower_(lower)
+			, theUpper_(upper)
+		{
+		}
+	public:
+		// Public functions and operators
+
+		auto Lower() const { return theLower_; }
+		auto Upper() const { return theUpper_; }
+		auto& Lower() { return theLower_; }
+		auto& Upper() { return theUpper_; }
+
+		auto IsInside(Standard_Real x) const
+		{
+			return INSIDE(x, Lower(), Upper());
+		}
+
+		void SetLower(Standard_Real lower)
+		{
+			theLower_ = lower;
+		}
+		void SetUpper(Standard_Real upper)
+		{
+			theUpper_ = upper;
+		}
+	};
 
 	typedef Global_Bound<Standard_Integer> intBound;
-
-	template<>
-	inline intBound::Global_Bound()
-		: theLower_(IntegerFirst())
-		, theUpper_(IntegerLast())
-	{}
-
 	typedef Global_Bound<Standard_Real> realBound;
 
-	template<>
-	inline realBound::Global_Bound()
-		: theLower_(RealFirst())
-		, theUpper_(RealLast())
-	{}
 }
 
 #endif // !_Global_Bound_Header
