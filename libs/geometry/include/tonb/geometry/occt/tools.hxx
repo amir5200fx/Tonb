9c6018ab9ce28cbf0f66c3cfe282b24de0357431
Amir Amir-Solaymani <aasoleimani86@gmail.com>
2025-08-25 23:12:09 +0100

    feat(geometry): implement OCCT 3D curve interpolation

---
+++ libs/geometry/include/tonb/geometry/occt/tools.hxx
@@ -0,0 +1,20 @@
+//
+// Created by amir on 8/24/25.
+//
+#pragma once
+#ifndef TONB_GEOMETRY_OCCT_TOOLS_HXX
+#define TONB_GEOMETRY_OCCT_TOOLS_HXX
+#include <tonb/geometry/pnt_3d.hxx>
+
+#include <vector>
+namespace tonb::geometry::occt {
+
+    // Forward Declarations
+    class Curve;
+    class Tools {
+    public:
+
+        static TNBGEOM_EXPORT Curve interpolate(const std::vector<Pnt3d>& qs, real tol = 1e-6);
+    };
+}
+#endif //TONB_GEOMETRY_OCCT_TOOLS_HXX
---
+++ libs/geometry/src/occt/tools.cxx
@@ -0,0 +1,46 @@
+//
+// Created by amir on 8/24/25.
+//
+#include <tonb/config.hxx>
+
+#if defined(TONB_WITH_OCCT)
+#include <tonb/geometry/occt/tools.hxx>
+#include <tonb/geometry/occt/curve.hxx>
+#include <tonb/geometry/occt/core/adapters.hxx>
+#include <tonb/geometry/occt/core/curve_helpers.hxx>
+
+#include <opencascade/GeomAPI_Interpolate.hxx>
+#include <opencascade/TColgp_HArray1OfPnt.hxx>
+#include <opencascade/gp_Pnt.hxx>
+#include <opencascade/StdFail_NotDone.hxx>
+
+namespace tonb::geometry::occt {
+
+    Curve Tools::interpolate(const std::vector<Pnt3d> &qs, const real tol) {
+        if (qs.empty()) {
+            throw std::runtime_error("Tools::interpolate: empty qs vector");
+        }
+        if (qs.size() < 2) {
+            throw std::runtime_error("Tools::interpolate: qs vector too small");
+        }
+        Handle(TColgp_HArray1OfPnt) points = new TColgp_HArray1OfPnt(1, static_cast<int>(qs.size()));
+        for (int i = 0; i < qs.size(); i++) {
+            points->SetValue(i + 1, core::to_gp(qs[i]));
+        }
+        try {
+            GeomAPI_Interpolate app(points, false, tol);
+            app.Perform();
+
+            return core::occt_curve_access::make(app.Curve());
+        } catch (const StdFail_NotDone &e) {
+            std::ostringstream oss;
+            oss<<"Tools::interpolate: failed to interpolate"<<'\n'
+            <<e.GetMessageString()<<'\n';
+            throw std::runtime_error(oss.str());
+        } catch (...) {
+            throw std::runtime_error("Tools::interpolate: failed to interpolate - Unknown error");
+        }
+    }
+
+}
+#endif
