bcb3cf6809e089d6eb2e9fd384490d0f1b5bef97
Amir Amir-Solaymani <aasoleimani86@gmail.com>
2025-08-25 22:48:42 +0100

    feat(geometry): forward declarations for occt entities

---
+++ libs/geometry/include/tonb/geometry/occt_fwd.hxx
@@ -0,0 +1,19 @@
+//
+// Created by amir on 8/24/25.
+//
+#pragma once
+#ifndef TONB_GEOMETRY_OCCT_FWD_HXX
+#define TONB_GEOMETRY_OCCT_FWD_HXX
+
+namespace tonb::geometry {
+    // Forward Declarations
+    namespace occt {
+        class Point;
+        class Vector;
+        class Axis;
+        class Transform;
+        class BBox;
+        class Curve;
+    }
+}
+#endif //TONB_GEOMETRY_OCCT_FWD_HXX
