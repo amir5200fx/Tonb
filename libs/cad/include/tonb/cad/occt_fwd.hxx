e5a12e3547f8b9aea5e91fcdf4787a7cb49b6cfb
Amir Amir-Solaymani <aasoleimani86@gmail.com>
2025-08-25 22:48:27 +0100

    feat(cad): forward declarations for occt entities

---
+++ libs/cad/include/tonb/cad/occt_fwd.hxx
@@ -0,0 +1,17 @@
+//
+// Created by amir on 8/24/25.
+//
+#pragma once
+#ifndef TONB_CAD_OCCT_FWD_HXX
+#define TONB_CAD_OCCT_FWD_HXX
+
+namespace tonb::cad {
+    // Forward Declarations
+    namespace occt {
+        class Body;
+        class Face;
+        class Edge;
+        class Vertex;
+    }
+}
+#endif //TONB_CAD_OCCT_FWD_HXX
