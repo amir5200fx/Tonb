39a787583a963867e552b7454bde72b18be7ad4e
amir5200fx <a.amirsolaymani@gmail.com>
2025-08-27 15:50:58 +0100

    feat(geom): Add alias for BBox OCCT wrap

---
+++ libs/cad/include/tonb/cad/bbox.hxx
@@ -0,0 +1,9 @@
+//
+// Created by amir on 8/27/25.
+//
+#pragma once
+#ifndef TONB_CAD_BBOX_HXX
+#define TONB_CAD_BBOX_HXX
+#include <tonb/cad/bbox_fwd.hxx>
+#include <tonb/geometry/occt/bbox.hxx>
+#endif //TONB_CAD_BBOX_HXX
---
+++ libs/cad/include/tonb/cad/bbox_fwd.hxx
@@ -0,0 +1,13 @@
+//
+// Created by amir on 8/27/25.
+//
+#pragma once
+#ifndef TONB_CAD_BBOX_FWD_HXX
+#define TONB_CAD_BBOX_FWD_HXX
+namespace tonb::geometry::occt {
+    class BBox;
+}
+namespace tonb::cad {
+    using BBox = geometry::occt::BBox;
+}
+#endif //TONB_CAD_BBOX_FWD_HXX
