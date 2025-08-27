e907ad34d62800d689aa420ade33e837128a64da
amir5200fx <a.amirsolaymani@gmail.com>
2025-08-27 16:01:44 +0100

    feat(cad): alias edge for OCCT edge wrapper

---
+++ libs/cad/include/tonb/cad/edge.hxx
@@ -0,0 +1,9 @@
+//
+// Created by amir on 8/27/25.
+//
+#pragma once
+#ifndef TONB_CAD_EDGE_HXX
+#define TONB_CAD_EDGE_HXX
+#include <tonb/cad/edge_fwd.hxx>
+#include <tonb/cad/occt/edge.hxx>
+#endif //TONB_CAD_EDGE_HXX
