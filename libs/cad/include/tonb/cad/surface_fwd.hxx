d6a3a62fea365d344d588e26f55525306f51cbbc
amir5200fx <a.amirsolaymani@gmail.com>
2025-08-29 13:29:14 +0100

    fix: declare Surface in geometry::occt namespace

--- libs/cad/include/tonb/cad/surface_fwd.hxx
+++ libs/cad/include/tonb/cad/surface_fwd.hxx
@@ -4,10 +4,10 @@
 #pragma once
 #ifndef TONB_CAD_SURFACE_FWD_HXX
 #define TONB_CAD_SURFACE_FWD_HXX
+namespace tonb::geometry::occt {
+    class Surface;
+}
 namespace tonb::cad {
-    namespace occt {
-        class Surface;
-    }
-    using Surface = occt::Surface;
+    using Surface = geometry::occt::Surface;
 }
 #endif //TONB_CAD_SURFACE_FWD_HXX
