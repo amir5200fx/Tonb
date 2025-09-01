af40539934f0cfe18ad63a39e0cb844c34ca4eb8
amir5200fx <a.amirsolaymani@gmail.com>
2025-09-01 22:17:39 +0100

    build: updating cmake files to support new features

---
+++ libs/geometry/src/spatial/impl.cxx
@@ -0,0 +1,6 @@
+//
+// Created by amir on 8/30/25.
+//
+//#include <tonb/geometry/spatial/adaptive_tree.hxx>
+//#include <tonb/geometry/spatial/balanced_quad_tree.hxx>
+#include <tonb/geometry/spatial/grid_2d.hxx>
