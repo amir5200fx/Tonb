b87c170c81a8e1449a01255fa4cee2d62babd780
amir5200fx <a.amirsolaymani@gmail.com>
2025-09-07 12:06:31 +0100

    fix(base): include correct log.hxx file

--- libs/system/include/tonb/system/task.hxx
+++ libs/system/include/tonb/system/task.hxx
@@ -7,7 +7,7 @@
 
 #include <tonb/system/module.hxx>
 #include <tonb/system/run_context.hxx>
-#include <tonb/system/log.hxx-backup>
+#include <tonb/system/log.hxx>
 
 #include <atomic>
 #include <chrono>
